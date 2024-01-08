08bca84091be099fa37aba1aefbafb3dbe0f94ec
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-01-08 23:23:01 +0900

    Title: 이상한 폭탄 2, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240108/이상한 폭탄 2/strange-bomb-2.py
+++ 240108/이상한 폭탄 2/strange-bomb-2.py
@@ -5,16 +5,28 @@ for _ in range(n):
     number=int(input())
     bombs.append(number)
 
-def in_range(number):
-    return number<n and number>=0
+# def in_range(number):
+#     return number<n and number>=0
 
-boom=[]
+# boom=[]
+# for i in range(n):
+#     for j in range(i-k,i+k+1):
+#         if in_range(j) and i!=j:
+#             if bombs[i]==bombs[j]:
+#                 boom.append(bombs[i])
+# if len(boom)==0:
+#     print(-1)
+# else:
+#     print(max(boom))
+ans=-1
 for i in range(n):
-    for j in range(i-k,i+k+1):
-        if in_range(j) and i!=j:
-            if bombs[i]==bombs[j]:
-                boom.append(bombs[i])
-if len(boom)==0:
-    print(-1)
-else:
-    print(max(boom))
+    for j in range(i+1,n):
+        if i-j>k:
+            break
+        
+        if bombs[i] != bombs[j]:
+            continue
+        
+        ans=max(ans,bombs[i])
+
+print(ans)

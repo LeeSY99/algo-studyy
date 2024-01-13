55d76f24d2f677ccae0c317b5f95b852758e361f
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-01-13 22:16:32 +0900

    Title: 팀으로 하는 틱택토 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240113/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,24 +1,33 @@
 board=[list(int(x) for x in input()) for _ in range(3)]
 
 
+team=[]
 count=0
 for b in board:
     if len(set(b))==2:
-        count+=1
+        if set(b) not in team:
+            team.append(set(b))
+            count+=1
 
 for i in range(3):
     now=[]
     for j in range(3):
         now.append(board[j][i])
     if len(set(now))==2:
-        count+=1
+        if set(now) not in team:
+            team.append(set(now))
+            count+=1
 
 now=[board[0][0],board[1][1],board[2][2]]
 if len(set(now))==2:
-    count+=1
+    if set(now) not in team:
+        team.append(set(now))
+        count+=1
 
 now=[board[0][2],board[1][1],board[2][0]]
 if len(set(now))==2:
-    count+=1
+    if set(now) not in team:
+        team.append(set(now))
+        count+=1
 
 print(count)

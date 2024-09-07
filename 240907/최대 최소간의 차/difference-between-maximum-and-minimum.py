d97a4c6b5e2aa7fcb571b19ce490f3cd4e10409c
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-07 14:48:04 +0900

    Title: 최대 최소간의 차, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240907/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240907/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -3,12 +3,12 @@ nums= list(map(int,input().split()))
 
 import sys
 cost=sys.maxsize
-for i in range(min(nums),max(nums)+1-k):
+for i in range(min(nums),max(nums)+1-k):#nums의 최소와 최대에서 k작은 구간만 탐색 ->
     now_cost=0
     for j in range(n):
-        if nums[j]<i:
+        if nums[j]<i:                   #nums를 순회하면서 예상 최소보다 작으면 비용추가
             now_cost+=(i-nums[j])
-        elif nums[j]>i+k:
+        elif nums[j]>i+k:               #예상 최대보다 크면 비용추가
             now_cost+=(nums[j]-(i+k))
     cost=min(cost,now_cost)
 

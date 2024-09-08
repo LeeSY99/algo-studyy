c56f6132ad120ac94b7a87bdc6e691788b284bfc
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-08 23:42:42 +0900

    Title: 구역 청소, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/구역 청소/district-cleaning.py
+++ 240908/구역 청소/district-cleaning.py
@@ -1,15 +1,15 @@
 a,b =map(int,input().split())
 c,d=map(int,input().split())
 
-# intersect=1
-# if b < c or d < a:
-#     intersect=0
+intersect=1
+if b < c or d < a:
+    intersect=0
 
-# if intersect:
-#     ans= (max(a,c)-min(a,c))+(max(b,d)-min)
+if intersect:
+    ans=max(b,d)-min(a,c)
     
-# else:
-#     ans=(d-c)+(b-a)
+else:
+    ans=(d-c)+(b-a)
     
-ans=max(b,d)-min(a,c)
+
 print(ans)

9f524f34a5b93507fedea1539465bd1963241534
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-08 23:12:37 +0900

    Title: 두 선분, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/두 선분/two-lines.py
+++ 240908/두 선분/two-lines.py
@@ -3,9 +3,9 @@ x1,x2,x3,x4 = map(int,input().split())
 def check(x1,x2,x3,x4):
     intersect=1
 
-    if x2<=x3:
+    if x2<x3:
         intersect=0
-    elif x4<=x1:
+    elif x4<x1:
         intersect=0
     
     if intersect:

b136baae35e275cc7ee465e9d172fa4defb7bc76
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-08 23:18:28 +0900

    Title: 두 직사각형, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/두 직사각형/two-rectangles.py
+++ 240908/두 직사각형/two-rectangles.py
@@ -1,15 +1,15 @@
 x1,y1,x2,y2 = map(int,input().split())
 a1,b1,a2,b2 = map(int,input().split())
 
-overlap=0
-if x2>a1:
-    overlap=1
-elif a2>x1:
-    overlap=1
-elif y2>b1:
-    overlap=1
-elif b2>y1:
-    overlap=1
+overlap=1
+if x2<a1:
+    overlap=0
+elif a2<x1:
+    overlap=0
+elif y2<b1:
+    overlap=0
+elif b2<y1:
+    overlap=0
 
 if overlap:
     print('overlapping')

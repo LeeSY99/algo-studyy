71064092da96b13b5cacf8b48affa982fdacb940
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-09 14:59:10 +0900

    Title: 코딩톡, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240909/코딩톡/coding-talk.py
@@ -0,0 +1,19 @@
+n,m,p=map(int,input().split())
+info = [list(input().split()) for _  in range(m)]
+
+people=[]
+start='A'
+for i in range(n):
+    people.append(chr(ord(start)+i))
+
+read=[]
+for i in range(p-1,m):
+    read.append(info[i][0])
+
+ans=[]
+for p in people:
+    if p not in read:
+        ans.append(p)
+ans.sort()
+for a in ans:
+    print(a, end=' ')

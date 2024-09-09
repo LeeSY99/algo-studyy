9f28b3739adfdf994b427f547236b4445dc3c57b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-09 16:37:37 +0900

    Title: X 달리기, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/X 달리기/x-run.py
+++ 240909/X 달리기/x-run.py
@@ -5,20 +5,21 @@ moved=[1]
 
 i=1
 while(1):
-    if (x//2) > moved[i-1]:
+    if (speed[i-1]+1) * (speed[i-1]+2) /2 <= x-moved[i-1]:
         now_speed=speed[i-1]+1
         speed.append(now_speed)
-        moved.append((moved[i-1]+now_speed))
-    elif  speed[i-1] <(x//2) - moved[i-1]< speed[i-1]+1:
+        moved.append(moved[i-1]+now_speed)
+    elif (speed[i-1]) * (speed[i-1]+1) /2 <= x-moved[i-1]:
         now_speed=speed[i-1]
         speed.append(now_speed)
-        moved.append((moved[i-1]+now_speed))
+        moved.append(moved[i-1]+now_speed)
     else:
         now_speed=max(1,speed[i-1]-1)
         speed.append(now_speed)
-        moved.append((moved[i-1]+now_speed))
-    if moved[i]>=x:
+        moved.append(moved[i-1]+now_speed)
+    if moved[i]==x:
         break
     i+=1
+
 
 print(len(moved))

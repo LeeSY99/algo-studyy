b187ef880458fa8351fc56b0ef722f1157a9c973
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-10 16:22:58 +0900

    Title: 연속된 숫자 만들기 3, Time: 2155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240910/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240910/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -4,13 +4,13 @@ count=0
 while(1):
     if b-a==1 and c-b==1:
         break
-    if b-a >= c-a:
-        a=b+1
-        a,b=b,a
-        count+=1
-    else:
+    if b-a >= c-b:
         c=b-1
         b,c=c,b
+        count+=1
+    else:
+        a=b+1
+        a,b=b,a
         count+=1
 
 print(count)

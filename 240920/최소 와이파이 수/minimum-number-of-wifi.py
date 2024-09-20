72742eaefb13eaec42679b1cef45b42e44b0ff1a
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-20 23:00:57 +0900

    Title: 최소 와이파이 수, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240920/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240920/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,7 +2,19 @@ n,m=map(int,input().split())
 
 live = list(map(int,input().split()))
 
-ans=n/(2*m+1)
-ans=ans+(1 if ans > int(ans) else 0)
+# if m==0:
+#     ans=sum(live)
+# else:
+ans=0
+index=0
+while(1):
+    if live[index]==1:
+        ans+=1
+        index+=2*m+1
+    else:
+        index+=1
+    if index>=n:
+        break
+
 
 print(int(ans))

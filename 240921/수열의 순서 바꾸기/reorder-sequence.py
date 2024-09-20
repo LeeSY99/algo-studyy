19431ca9fce7165e88dc4b4418efde84caddbb61
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-09-21 00:22:04 +0900

    Title: 수열의 순서 바꾸기, Time: 183ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240921/수열의 순서 바꾸기/reorder-sequence.py
+++ 240921/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,13 +1,14 @@
 n=int(input())
 nums=list(map(int,input().split()))
 
-max_num,max_index=0,0
-for i in range(n):
-    if nums[i]>max_num:
-        max_num=nums[i]
-        max_index=i
+ans=0
+for i in range(n-1):
+    ok=True
+    for j in range(i,n-1):
+        if nums[j]>nums[j+1]:
+            ok=False
+            ans+=1
+    if ok:
+        break
 
-if max_index==n-1:
-    print(0)
-else:  
-    print(max_index+1)
+print(ans)

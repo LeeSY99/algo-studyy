c9e641ac33bdbb12396727e5726417d8cc6ed312
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2024-12-27 05:23:26 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241227/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 241227/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,6 +6,9 @@ for i in range(len(string)):
     if string[i]=='(':
         mystack.append('(')
     else:
+        if len(mystack)==0:
+            print('No')
+            quit()
         mystack.pop()
 
 if len(mystack)==0:

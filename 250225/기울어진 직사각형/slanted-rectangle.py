fd109e20308f7e372849ac67feb909066cfa040c
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-02-25 23:20:19 +0900

    Title: 기울어진 직사각형, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250225/기울어진 직사각형/slanted-rectangle.py
+++ 250225/기울어진 직사각형/slanted-rectangle.py
@@ -9,14 +9,23 @@ def in_range(i,j):
 ans=0
 for i in range(n):
     for j in range(n):
-        if in_range(i,j) and in_range(i-1,j-1):
-            rec=grid[i][j] + grid[i-1][j-1]
-            # is_rec=False
-            for k in range(1,n):
-                if in_range(i-k,j+k) and in_range(i-1-k,j-1+k):
-                    is_rec=True
-                    rec +=(grid[i-k][j+k] + grid[i-1-k][j-1+k])
-                    ans = max(ans, rec)
+        for a in range(2,n): #세로길이
+            for b in range(2,n): #가로길이
+                num=0
+                is_rec=True
+                for h in range(a):
+                    for l in range(b):
+                        if (h==0) or (h==a-1) or (l==0) or (l==b-1):
+                            if in_range(i+h-l,j+h+l):
+                                num+=(grid[i+h-l][j+h+l])
+                            else:
+                                is_rec=False
+                                break
+                    if is_rec:
+                        ans=max(ans,num)
+                
+        
+            
 
 print(ans)
 

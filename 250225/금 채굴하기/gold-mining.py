a71412d2413c80357645bb96bcb5d8a527353b4e
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-02-25 21:35:47 +0900

    Title: 금 채굴하기, Time: 51ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250225/금 채굴하기/gold-mining.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Write your code here!
+def in_range(i,j):
+    return 0 <= i <n and 0 <= j <n
+
+def diamond(i,j,k): #마름모 탐색
+    gold=0
+    for a in range(n):
+        for b in range(n):
+            if abs(a-i) + abs(b-j) <= k:
+                if grid[a][b]:
+                    gold+=1
+    return gold
+
+
+ans=0
+for k in range(n//2+1):
+    for i in range(n):
+        for j in range(n): ##중심점 
+            gold=diamond(i,j,k)
+            ans = max(ans,gold)
+
+print(ans)
+        

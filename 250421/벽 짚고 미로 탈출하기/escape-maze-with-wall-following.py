273bf23faf8cd40ce40d27c872c69fc5731d2f29
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-04-21 16:45:12 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250421/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 250421/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -12,29 +12,33 @@ for i in range(1, N + 1):
 dx, dy = [1,0,-1,0], [0,-1,0,1]
 
 def in_range(r,c):
-    return 0<=r<N and 0<=c<N
+    return 0<r<=N and 0<c<=N
 
 def solve():
     global x,y
     time = 0
     i = 0
-    x ,y = x-1, y-1
+    x,y = y,x
     while 1:
         time+=1
-        grid[x][y]=0
+        grid[y][x]=0
         while 1:
             count = 0
             if in_range(x+dx[i], y+dy[i]):
-                if grid[x+dx[i]][y+dy[i]] == '#':
+                if grid[y+dy[i]][x+dx[i]] == '#':
                     count+=1
                     i = (i+1) % 4
                     if count>=4:
                         return -1
-                elif grid[x+dx[i]][y+dy[i]] == '.':
+                elif grid[y+dy[i]][x+dx[i]] == '.':
                     x = x+dx[i]
                     y = y+dy[i]
+                    nextx = x+ dx[(i-1)%4]
+                    nexty = y+ dy[(i-1)%4]
+                    if in_range(nextx,nexty) and grid[nexty][nextx] != '#':
+                        i = (i-1)%4
                     break
-                elif grid[x+dx[i]][y+dy[i]] == '0':
+                elif grid[y+dy[i]][x+dx[i]] == 0:
                     return -1
             else:
                 return time

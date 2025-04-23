efe9f4851a1527880b11d29c96219cb442175a82
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-04-23 13:29:18 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250423/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,52 @@
+n = int(input())
+x, y = map(int, input().split())
+
+grid = [['#'] * (n+2) for _ in range(n+2)]
+for i in range(1,n+1):
+    line = input()
+    for j, ch in enumerate(line, start=1):
+        grid[i][j] = ch
+
+dr = [0,1,0,-1]
+dc = [1,0,-1,0]
+
+def in_range(r,c):
+    return 1<=r<=n and 1<=c<=n
+
+def solve():
+    r,c = x,y
+    d = 0
+    time = 0
+    visited =set()
+
+    while 1:
+        #탈출불가능
+        if (r,c,d) in visited:
+            return -1
+        visited.add((r,c,d))
+
+        #다음 칸 검사
+        rd = (d+1)%4
+        nr, nc = r+dr[rd], c+dc[rd]
+
+        if not in_range(nr,nc): #탈출
+            return time+1
+        
+        if grid[nr][nc] == '.': #진행
+            d=rd
+            r,c=nr,nc
+            time+=1
+            continue
+        
+        #다음칸이 벽이면
+        nr, nc = r+dr[d], c+dc[d]
+        if not in_range(nr,nc):
+            return time+1
+        
+        if grid[nr][nc] == '.':
+            r,c= nr,nc
+            time+=1
+            continue
+        d=(d+3)%4
+    
+print(solve())

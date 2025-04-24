6db7fc1ce129973788480659f27815a41a65a6b1
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-04-24 11:15:33 +0900

    Title: 뱀은 사과를 좋아해, Time: 108ms, Memory: 19MB, Status: Passed - Codetree

--- 250424/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 250424/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -14,11 +14,29 @@ for _ in range(k):
 def in_range(r,c):
     return 0<=r<n and 0<=c<n
 
+def tail_move(i,j,er,ec):
+    d = directions[i][0]
+    if d == 'R':
+        grid[er][ec] = 0
+        er,ec = er,ec+1
+    elif d == 'D':
+        grid[er][ec] = 0
+        er,ec = er+1,ec
+    elif d == 'L':
+        grid[er][ec] = 0
+        er,ec = er,ec-1
+    elif d == 'U':
+        grid[er][ec] = 0
+        er,ec = er-1,ec
+    return er,ec
+
 def solve():
     grid[0][0]=1
     sr, sc = 0,0
     er, ec = 0,0
     time = 0
+    taili = 0
+    tailp = 0
     for d, p in directions:
         for i in range(p):
             time+=1
@@ -26,50 +44,62 @@ def solve():
                 sr,sc = sr,sc+1
                 if not in_range(sr,sc):
                     return time
-                if grid[sr][sc] == 0:
-                    grid[sr][sc] = 1
-                    grid[er][ec] = 0
-                    er,ec = er,ec+1
+                if grid[sr][sc] != 2:
+                    er,ec = tail_move(taili,tailp,er,ec)
+                    tailp+=1
+                    if tailp == directions[taili][1]:
+                        taili += 1
+                        tailp = 0
+                    if grid[sr][sc] == 0:
+                        grid[sr][sc] = 1
+                    else: return time
                 elif grid[sr][sc] == 2:
                     grid[sr][sc] = 1
-                elif grid[sr][sc] == 1:
-                    return time
             elif d == 'D':
                 sr,sc = sr+1,sc
                 if not in_range(sr,sc):
                     return time
-                if grid[sr][sc] == 0:
-                    grid[sr][sc] = 1
-                    grid[er][ec] = 0
-                    er,ec = er+1,ec
+                if grid[sr][sc] != 2:
+                    er,ec = tail_move(taili,tailp,er,ec)
+                    tailp+=1
+                    if tailp == directions[taili][1]:
+                        taili += 1
+                        tailp = 0
+                    if grid[sr][sc] == 0:
+                        grid[sr][sc] = 1
+                    else: return time
                 elif grid[sr][sc] == 2:
                     grid[sr][sc] = 1
-                elif grid[sr][sc] == 1:
-                    return time
             elif d == 'L':
                 sr,sc = sr,sc-1
                 if not in_range(sr,sc):
                     return time
-                if grid[sr][sc] == 0:
-                    grid[sr][sc] = 1
-                    grid[er][ec] = 0
-                    er,ec = er,ec-1
+                if grid[sr][sc] != 2:
+                    er,ec = tail_move(taili,tailp,er,ec)
+                    tailp+=1
+                    if tailp == directions[taili][1]:
+                        taili += 1
+                        tailp = 0
+                    if grid[sr][sc] == 0:
+                        grid[sr][sc] = 1
+                    else: return time
                 elif grid[sr][sc] == 2:
                     grid[sr][sc] = 1
-                elif grid[sr][sc] == 1:
-                    return time
             elif d == 'U':
                 sr,sc = sr-1,sc
                 if not in_range(sr,sc):
                     return time
-                if grid[sr][sc] == 0:
-                    grid[sr][sc] = 1
-                    grid[er][ec] = 0
-                    er,ec = er-1,ec
+                if grid[sr][sc] != 2:
+                    er,ec = tail_move(taili,tailp,er,ec)
+                    tailp+=1
+                    if tailp == directions[taili][1]:
+                        taili += 1
+                        tailp = 0
+                    if grid[sr][sc] == 0:
+                        grid[sr][sc] = 1
+                    else: return time
                 elif grid[sr][sc] == 2:
                     grid[sr][sc] = 1
-                elif grid[sr][sc] == 1:
-                    return time
     return time
 
 print(solve())        

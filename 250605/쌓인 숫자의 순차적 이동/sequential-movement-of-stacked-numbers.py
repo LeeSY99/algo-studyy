c954a2246e0015ef02548ac8d01586c70150fb8b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-06-05 16:31:53 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250605/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
+++ 250605/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -1,28 +1,64 @@
 n, m = map(int, input().split())
-nums = [list(map(int, input().split())) for _ in range(n)]
+nums = [
+    [[x] for x in map(int, input().split())] 
+    for _ in range(n)
+    ]
 a = list(map(int, input().split()))
 
 dr,dc = [0,1,1,1,0,-1,-1,-1,],[1,1,0,-1,-1,-1,0,1]
 def in_range(r,c):
     return 0<=r<n and 0<=c<n
 
+
 for num in a:
+    temp = []
+    ok = False
     for i in range(n):
         for j in range(n):
-            if nums[i][j] == num:
-                max_num = 0
-                for r, c in zip(dr,dc):
-                    nr, nc = i+nr, j+nc
-                    if in_range(nr,nc):
-                        if nums[nr][nc][0] > max_num:
-                            maxr, maxc = nr,nc
-                            max_num = nums[nr][nc]
-                if max_num != 0:
-                    while nums[i][j]:
-                        nums[maxr][maxc].append(nums[i][j].pop())
+            k=0
+            for _ in range(len(nums[i][j])-1,-1,-1):
+                k += 1
+                if nums[i][j][_] == num:
+                    for m in range(k):
+                        now_value = nums[i][j].pop()
+                        max_num = 0
+                        maxr, maxc = 0,0
+                        for d in range(8):
+                            nr = i + dr[d]
+                            nc = j + dc[d]
+
+                            if not in_range(nr,nc):
+                                continue
+
+                            if nums[nr][nc] and nums[nr][nc][0] > max_num:
+                                maxr, maxc = nr, nc
+                                max_num = nums[nr][nc][0]
+                        if max_num == 0:
+                            nums[i][j].append(now_value)
+                            continue
+                        temp.append(now_value)
+                        if now_value == num:
+                            while temp:
+                                nums[maxr][maxc].append(temp.pop())
+                            ok = True
+                            break
+                if ok:
+                    break
+            if ok:
+                break
+        if ok:
+            break
+
+                
+
 
 for i in range(n):
     for j in range(n):
-        print(*nums[i][j])
+        if not nums[i][j]:
+            print('None')
+        else:
+            while nums[i][j]:
+                print(nums[i][j].pop(), end= ' ')
+            print()
 
 

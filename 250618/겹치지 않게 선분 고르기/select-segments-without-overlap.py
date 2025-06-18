e0a07da701862797c7a84d55a1757dfcb1c08951
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-06-18 18:07:49 +0900

    Title: 겹치지 않게 선분 고르기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250618/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,40 @@
+n = int(input())
+
+lines=[]
+for i in range(n):
+    a ,b = map(int, input().split())
+    lines.append((a,b))
+
+ans = 0
+
+select=[]
+def choose(a):
+    global ans
+    global lines
+    if a == n:
+        choosed=[]
+        for i in range(n):
+            if select[i] == 1:
+                choosed.append(lines[i])
+        count = check(choosed)
+        ans = max(ans, count)
+        return
+    select.append(0)
+    choose(a+1)
+    select.pop()
+
+    select.append(1)
+    choose(a+1)
+    select.pop()
+
+def check(choosed):
+    count = 0
+    end = -1
+    for a, b in choosed:
+        if b>end:
+            count+=1
+            end = b
+    return count        
+
+choose(0)
+print(ans)

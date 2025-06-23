5e493d772b6a98b857e3021d389d198add78b856
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-06-23 16:08:58 +0900

    Title: 최소 점프 횟수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250623/최소 점프 횟수/min-num-of-jumps.py
+++ 250623/최소 점프 횟수/min-num-of-jumps.py
@@ -10,13 +10,13 @@ def backtrack(count,index):
         return
     elif index > n-1:
         return
-    if jump[index] == 0:
-        print(-1)
-        quit()
     
 
     for i in range(1, jump[index]+1):
         backtrack(count+1,index+i)
 
 backtrack(0,0)
-print(ans)
+if ans == sys.maxsize:
+    print(-1)
+else:
+    print(ans)

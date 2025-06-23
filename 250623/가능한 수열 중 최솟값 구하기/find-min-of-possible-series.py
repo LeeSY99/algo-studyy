55ba8c35128a9ec3da8ca7e561200a1257d6ea33
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-06-23 15:44:34 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 250623/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 250623/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -1,39 +1,26 @@
 n = int(input())
+p = []
 
+def backtrack(count):
+    # global p
+    if count == n:
+        for num in p:
+            print(num, end = '')   
+        quit() 
+    
+    for i in range(4,7):
+        p.append(i)
+        if check():
+            backtrack(count+1)  
+        p.pop()
 
-def solve():
-    p = []
-    def backtrack(count):
-        nonlocal p
-        if count == n:
-            if check():
-                for num in p:
-                    print(num, end = '')   
-                quit() 
-            return
+def check():
+    l = len(p)
+    for gap in range(1,l//2+1):
+        for i in range(l - 2 * gap+1):
+            if p[i:i+gap] == p[i+gap:i+2*gap]:
+                return False
+    return True
 
-        for i in range(4,7):
-            p.append(i)
-            backtrack(count+1)
-            p.pop()
-
-    def check():
-        for i in range(n):
-            for j in range(i+1, n):
-                gap = j-i
-                if gap > n-j:
-                    continue
-                count = 0
-                for k in range(gap):
-                    if p[i+k] == p[j+k]:
-                        count+=1
-                if count == gap:
-                    return False
-        return True
-
-    a = backtrack(0)
-    # for num in a:
-    #     print(num, end = '')
-
-solve()
+backtrack(0)
 

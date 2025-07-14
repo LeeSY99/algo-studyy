c85619985da14bfdbaf28ff97d71e24ec77f2a8b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-07-14 19:29:55 +0900

    Title: 대응되는 수와 문자, Time: 329ms, Memory: 42MB, Status: Passed - Codetree

--- 250714/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 250714/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,16 +1,17 @@
 n,m = map(int,input().split())
 
 mydict = dict()
+mydict2 = dict()
 
 for i in range(1, n+1):
     alpha = input()
-    mydict[alpha] = str(i)
+    mydict[alpha] = i
+    mydict2[i] = alpha
 
 for _ in range(m):
     k = input()
     if k in mydict:
         print(mydict[k])
     else:
-        for key, value in mydict.items():
-            if value == k:
-                print(key)
+        print(mydict2[int(k)])
+        

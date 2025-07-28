48aa97a7abe11d56fe640995afc3ddb515ae0d65
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-07-28 10:23:44 +0900

    Title: 떨어지는 1자 블록, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250728/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,22 @@
+n,m,k = map(int, input().split())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+sj = k-1
+ej = k-1+m-1
+def in_range(r,c):
+    return 0<=r<n and 0<=c<n
+
+for i in range(n):
+    ok = True
+    for j in range(sj, ej+1):
+        if in_range(i+1,j) and grid[i+1][j] == 1:
+            ok = False
+
+    if not ok:
+        for j in range(sj, ej+1):
+            grid[i][j] = 1
+        break
+
+for a in grid:
+    print(*a)

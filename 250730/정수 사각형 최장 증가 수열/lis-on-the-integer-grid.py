9e4f0cfd4a56290e5a61d5c0346f080912a9702a
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-07-30 12:54:00 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 180ms, Memory: 26MB, Status: Runtime Error - Codetree

--- 250730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 250730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,7 +1,7 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[1] * n for _ in range(n)]
+dp = [[-1] * n for _ in range(n)]
 
 dis,djs = [0,0,-1,1],[1,-1,0,0]
 
@@ -9,18 +9,23 @@ def in_range(i,j):
     return 0<=i<n and 0<=j<n
 
 ans = 0
-def bfs(i,j):
-    global ans
-    for di, dj in zip(dis, djs):
-            ni, nj = i+di, j+dj
-            if in_range(ni,nj) and grid[ni][nj] > grid[i][j]:
-                dp[ni][nj] = max(dp[i][j] + 1, dp[ni][nj])
-                ans = max(ans,dp[ni][nj])
-                bfs(ni,nj)
+from collections import deque
+q = deque()
+
+def dfs(i,j):
+    if dp[i][j] != -1:
+        return dp[i][j]
+    
+    dp[i][j] = 1
+    for di, dj in zip(dis,djs):
+        ni, nj = i+di, j+dj
+        if in_range(ni,nj) and grid[ni][nj] > grid[i][j]:
+            dp[i][j] = max(dp[i][j], dfs(ni,nj)+1)
+    return dp[i][j]
 
 for i in range(n):
     for j in range(n):
-        bfs(i,j)
+        ans = max(ans, dfs(i, j))
         
         
 print(ans)

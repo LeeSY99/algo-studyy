e016d8452c04f67cd5e04265795b107eb965aef6
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-07-30 15:38:02 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 554ms, Memory: 58MB, Status: Passed - Codetree

--- 250730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 250730/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -4,33 +4,60 @@ sys.setrecursionlimit(10**6)
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[-1] * n for _ in range(n)]
+dp = [[1] * n for _ in range(n)]
 
 dis,djs = [0,0,-1,1],[1,-1,0,0]
 
 def in_range(i,j):
     return 0<=i<n and 0<=j<n
 
-ans = 0
-from collections import deque
-q = deque()
-
-def dfs(i,j):
-    if dp[i][j] != -1:
-        return dp[i][j]
-    
-    dp[i][j] = 1
-    for di, dj in zip(dis,djs):
-        ni, nj = i+di, j+dj
-        if in_range(ni,nj) and grid[ni][nj] > grid[i][j]:
-            dp[i][j] = max(dp[i][j], dfs(ni,nj)+1)
-    return dp[i][j]
+cells = []
 
 for i in range(n):
     for j in range(n):
-        ans = max(ans, dfs(i, j))
-        
-        
+        cells.append((grid[i][j],i,j))
+
+cells.sort()
+
+for _,i,j in cells:
+    for di, dj in zip(dis,djs):
+        ni, nj = i+di, j+dj
+        if in_range(ni,nj) and grid[ni][nj] > grid[i][j]:
+            dp[ni][nj] = max(dp[ni][nj], dp[i][j]+1)
+
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        ans = max(ans, dp[i][j])
 print(ans)
+
+
+
+
+
+
+# -----------------------dfs-------------------------
+# ans = 0
+# from collections import deque
+# q = deque()
+
+# def dfs(i,j):
+#     if dp[i][j] != -1:
+#         return dp[i][j]
+    
+#     dp[i][j] = 1
+#     for di, dj in zip(dis,djs):
+#         ni, nj = i+di, j+dj
+#         if in_range(ni,nj) and grid[ni][nj] > grid[i][j]:
+#             dp[i][j] = max(dp[i][j], dfs(ni,nj)+1)
+#     return dp[i][j]
+
+# for i in range(n):
+#     for j in range(n):
+#         ans = max(ans, dfs(i, j))
+        
+        
+# print(ans)
 # for d in dp:
 #     print(*d)

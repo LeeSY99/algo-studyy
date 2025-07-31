a9c3c5c1eb36649dd39e4858e853ab60492203e9
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-07-31 15:45:20 +0900

    Title: 최대 점프 횟수, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250731/최대 점프 횟수/maximum-number-of-jumps.py
+++ 250731/최대 점프 횟수/maximum-number-of-jumps.py
@@ -1,15 +1,15 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
-dp = [0] * n
+dp = [-1] * n
+dp[0] = 0
 
 for i in range(1,n):
     for j in range(i):
-        if j+nums[j] >= i:
+        if dp[j] != -1 and j+nums[j] >= i:
             dp[i] = max(dp[i], dp[j]+1)
-    if dp[i] == 0:
-        break
+    # if dp[i] == 0:
+    #     break
 
-# print(dp)
-ans = 1
-print(max(ans,dp[n-1]))
+# print(*dp)
+print(max(dp))

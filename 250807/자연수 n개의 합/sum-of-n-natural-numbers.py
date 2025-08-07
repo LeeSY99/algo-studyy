295f2b49f673fdcde33c1c6d95ae1dc8bea26f9e
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-07 23:43:43 +0900

    Title: 자연수 n개의 합, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250807/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -0,0 +1,15 @@
+s = int(input())
+
+left = 1
+right = 10**18
+max_num = 0
+
+while left <= right:
+    mid = (left+right)//2
+    if mid * (mid+1) //2 <= s:
+        left = mid + 1
+        max_num = max(max_num, mid)
+    else:
+        right = mid - 1
+
+print(max_num)

ff2aa84828f1926e22e25810ef537ebbbc3fdd75
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 00:59:19 +0900

    Title: 삼 오 무, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250808/삼 오 무/three-five-moo.py
+++ 250808/삼 오 무/three-five-moo.py
@@ -1,20 +1,20 @@
 n = int(input())
 
 left = 0
-right = 10**9 *15
+right = 10**10 
 
 
 def check(num):
     return num - num//3 - num//5 + num//15
 
+ans = 10**10
 while left<=right:
     mid = (left + right) // 2
-    if check(mid) < n:
-        left = mid +1
-    elif check(mid) > n:
+    if check(mid) >= n:
+        ans = min(ans, mid)
         right = mid-1
     else:
-        print(mid)
-        break
-
+        left = mid+1
+    
+print(ans)
     

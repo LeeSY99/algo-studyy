ea890bf66e4ec47951ea6f466f9c5252e368e975
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 13:44:07 +0900

    Title: 최소 통과 시간, Time: 91ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 250808/최소 통과 시간/minimum-transit-time.py
@@ -0,0 +1,29 @@
+n,m = map(int, input().split())
+
+time = []
+for _ in range(m):
+    a = int(input())
+    time.append(a)
+
+left = 1
+right = 10**9
+ans = 10**9
+
+def check(mid):
+    ps = 0
+    for t in time:
+        ps += mid//t
+
+    return ps
+
+while left <= right:
+    mid = (left + right) //2
+
+
+    if check(mid) >= n:
+        ans = min(ans, mid)
+        right = mid -1
+    else:
+        left = mid + 1
+
+print(ans)

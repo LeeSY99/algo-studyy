3e2cd45ff03912a34cede9ef603632a4914e0326
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 21:26:35 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 348ms, Memory: 38MB, Status: Wrong Answer - Codetree

---
+++ 250808/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -0,0 +1,23 @@
+n, q = map(int, input().split())
+
+points = list(map(int, input().split()))
+
+arr = [0] * 1000001
+prefix_sum = [0] * 1000001
+for point in points:
+    arr[point] = 1
+
+for i in range(1,1000001):
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
+
+
+for _ in range(q):
+    count = 0
+    a,b = map(int, input().split())
+    count += prefix_sum[b]-prefix_sum[a-1]
+        
+    print(count)
+    
+
+
+

534a173a05929c495da79f9acf57db13aa474a40
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 17:13:06 +0900

    Title: 최대 동전 거슬러주기, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250808/최대 동전 거슬러주기/max-coin-change.py
+++ 250808/최대 동전 거슬러주기/max-coin-change.py
@@ -2,14 +2,16 @@ n, m = map(int, input().split())
 coins = list(map(int, input().split()))
 
 #dp[i] = i원을 거슬러줄 최대 동전의 개수
-dp = [0] * (m+1)
+import sys
+dp = [-sys.maxsize] * (m+1)
+dp[0] = 0
 
 for i in range(1,m+1):
     for coin in coins:
-        if i-coin >=0:
+        if i-coin >= 0 and dp[i-coin] != -sys.maxsize:
             dp[i] = max(dp[i], dp[i-coin] + 1)
 
 ans = dp[m]
-if ans == 0:
+if ans == -sys.maxsize:
     ans = -1
 print(ans)

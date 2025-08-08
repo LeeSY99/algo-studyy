92a43658a4ffa198e2eaa79a1308e20b3c063838
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 16:00:40 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250808/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -0,0 +1,28 @@
+''' n개의 숫자 가장 긴 증가-감소 부분 수열 길이'''
+
+n = int(input())
+num = list(map(int, input().split()))
+
+''' dp[i][0] i번째 수에서 증가하고 잇음
+    dp[i][1] i번째 수에서 감소하고 잇음'''
+dp = [[0,0] for _ in range(n)]
+dp[0][0] = 1
+dp[0][1] = 1
+# print(dp)
+
+for i in range(1,n):
+    for j in range(i):
+        if num[j] < num[i]:
+            dp[i][0] = max(dp[i][0], dp[j][0] + 1)
+        elif num[j] > num[i]:
+            dp[i][1] = max(dp[i][1], dp[j][1] + 1, dp[j][0] + 1)
+
+# for d in dp:
+#     print(*d)
+
+ans = 0
+for d in dp:
+    ans = max(ans, d[0])
+    ans = max(ans, d[1])
+
+print(ans)

7237eead551794983a7a796b02456467ca661e52
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 22:02:34 +0900

    Title: 최대 직사각형 합, Time: 326ms, Memory: 21MB, Status: Passed - Codetree

--- 250808/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 250808/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -13,14 +13,26 @@ for i in range(1,n+1):
     for j in range(1,n+1):
         prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + nums[i][j]
 
+def get_sum(x1,y1,x2,y2):
+    return prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1]
+
+def get_max_area(x1,x2):
+    dp = [0] * (n+1)
+    for y in range(1,n+1):
+        value = get_sum(x1,y,x2,y)
+        dp[y] = max(value, dp[y-1] + value)
+
+    max_area = -sys.maxsize
+    for y in range(1,n+1):
+        max_area = max(max_area, dp[y])
+
+    return max_area
+
 import sys
 ans = -sys.maxsize
-for ei in range(1,n+1):
-    for ej in range(1,n+1):
-        for si in range(1,ei+1):
-            for sj in range(1,ej+1):
-                sq_sum = prefix_sum[ei][ej] - prefix_sum[si-1][ej] - prefix_sum[ei][sj-1] + prefix_sum[si-1][sj-1]
-                ans = max(ans, sq_sum)
+for i in range(1,n+1): # 시작 행
+    for j in range(i,n+1): # 끝 행
+        ans = max(ans, get_max_area(i,j))
 print(ans)
 # for p in prefix_sum:
 #     print(*p)

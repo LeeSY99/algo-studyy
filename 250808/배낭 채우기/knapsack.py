e7c24acf0da12f3e211b875f581a4da1d998ca3f
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 17:57:14 +0900

    Title: 배낭 채우기, Time: 89ms, Memory: 25MB, Status: Passed - Codetree

--- 250808/배낭 채우기/knapsack.py
+++ 250808/배낭 채우기/knapsack.py
@@ -8,19 +8,22 @@ for _ in range(n):
     weight.append(w)
     value.append(v)
 
-#n번쨰 보석 고려시 무게, 최대value
-dp = [[0,0] for _ in range(n+1)]
+#dp[i][j\]: i보석 고려. 고른 보석의 무게 합j -> 얻는 최대가치
+dp = [[0] * (m+1) for _ in range(n+1)]
 
+#1) i번보석을 배낭에 넣음
+#2) i번보석을 선택하지 않음
 for i in range(1,n+1):
-    for j in range(i):
-        if dp[j][0] + weight[i] <= m:
-            if dp[j][1] + value[i] > dp[i][1]:
-                dp[i][0] = dp[j][0] + weight[i]
-                dp[i][1] = dp[j][1] + value[i]
+    for j in range(m+1):
+        if j >= weight[i]:
+            dp[i][j] = max(dp[i-1][j-weight[i]] + value[i], dp[i-1][j] )
+        else:
+            dp[i][j] = dp[i-1][j]
+        
 
 ans = 0
-for d in dp:
-    ans = max(ans, d[1])
+for j in range(m+1):
+    ans = max(ans, dp[n][j])
 
 print(ans)
 

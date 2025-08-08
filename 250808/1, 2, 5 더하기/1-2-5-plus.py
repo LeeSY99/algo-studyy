de2c282e6cffafd6891238e74f9732a4454723aa
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-08 17:02:35 +0900

    Title: 1, 2, 5 더하기, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250808/1, 2, 5 더하기/1-2-5-plus.py
+++ 250808/1, 2, 5 더하기/1-2-5-plus.py
@@ -4,10 +4,11 @@ nums = [1,2,5]
 
 #dp[i] = 합이 i인 식의 가지수
 dp = [0] * (n+1)
+dp[0] = 1
 
 for i in range(1,n+1):
     for num in nums:
         if i-num >=0:
-            dp[i] += (dp[i-num]+1)
+            dp[i] = (dp[i] + dp[i-num])%10007
 
-print((dp[n]-dp[n-1])%10007)
+print(dp[n])

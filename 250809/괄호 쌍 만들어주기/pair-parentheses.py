04e9db1c721fd8d43bab67f6b6dc50b9302e2be9
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-09 14:38:31 +0900

    Title: 괄호 쌍 만들어주기, Time: 67ms, Memory: 22MB, Status: Passed - Codetree

--- 250809/괄호 쌍 만들어주기/pair-parentheses.py
+++ 250809/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,18 +1,18 @@
 a = list(input())
 n = len(a)
 
-open_ = [0] * n
-close_ = [0] * n
+r = [0] * (n)
 
-for i in range(n-1,0,-1):
-    if a[i] == ')' and a[i-1] == a[i]:
-        close_[i] += 1
+for i in range(n-2,-1,-1):
+    if a[i] == ')' and a[i] == a[i+1]:
+        r[i] = r[i+1]+1
+    else:
+        r[i] = r[i+1]
 
-for i in range(n-1):
+ans = 0
+for i in range(n-2):
     if a[i] == '(' and a[i] == a[i+1]:
-        open_[i] += 1
+        ans += r[i+2]
 
-# print(open_)
-# print(close_)
-
-print(sum(open_) * sum(close_))
+print(ans)
+    

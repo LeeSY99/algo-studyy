77e5fd7be081f16c641e439ede3d811316cc1b6d
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-09 13:59:49 +0900

    Title: 정원 입장은 선착순, Time: 1061ms, Memory: 30MB, Status: Time Limit Exceed - Codetree

---
+++ 250809/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,36 @@
+'''번호표 1~n n명의 사람
+i번 사람은 a 시간에 입구 도착, t시간 머무르다 감
+1명만 들어갈 수 있음
+i번사람이 도착했을때 안에 사람있으면 나올떄까지 기다려야함 '''
+import heapq
+n = int(input())
+people = []
+for i in range(1,n+1):
+    a,t = map(int, input().split())
+    people.append((i,a,t))
+
+people.sort(key = lambda x:x[1])
+ans = 0
+time = 0
+waiting = []
+
+heapq.heappush(waiting, people.pop(0))
+# print(people)
+
+while waiting:
+    id, a, t = heapq.heappop(waiting)
+    
+    wait_time = max(0, time-a)
+    ans = max(ans, wait_time)
+    time = a+t+wait_time
+    # print(f'{id} 입장, 대기{wait_time}, 퇴장시간{time}')
+    
+    
+    while people and people[0][1] <= time:
+        heapq.heappush(waiting, people.pop(0))
+
+    if people and not waiting and people[0][1] > time:
+        heapq.heappush(waiting, people.pop(0))
+    # print(waiting)
+
+print(ans)

e1deeed368cf8d34e2d37cb36d9b11da79c1dc8f
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-11 14:53:49 +0900

    Title: 트리 위에 물건 놓기, Time: 613ms, Memory: 98MB, Status: Runtime Error - Codetree

---
+++ 250811/트리 위에 물건 놓기/node-best-count.py
@@ -0,0 +1,36 @@
+n = int(input())
+graph = [[] for _ in range(n+1)]
+parent = [0] * (n + 1)
+visited = [False] * (n + 1)
+
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+import sys
+#dp[i][0] 물건 안놓음 dp[i][1] 놓음
+dp = [[0]*2 for _ in range(n+1)]
+
+def dfs(u):
+    for v in graph[u]:
+        if not visited[v]:
+            visited[v] = True
+            parent[v] = u
+            dfs(v)
+
+    dp[u][0] = 0
+    dp[u][1] = 1
+
+    for v in graph[u]:
+        if parent[v] != u:
+            continue
+
+        dp[u][0] += dp[v][1]
+        dp[u][1] += min(dp[v][0], dp[v][1])
+
+visited[1] = True
+dfs(1)
+
+
+print(min(dp[1][0], dp[1][1]))

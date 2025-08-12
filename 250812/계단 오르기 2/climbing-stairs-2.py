d0947872ae551530fa498b4d00f6beffc1e37c55
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-12 22:40:04 +0900

    Title: 계단 오르기 2, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 250812/계단 오르기 2/climbing-stairs-2.py
+++ 250812/계단 오르기 2/climbing-stairs-2.py
@@ -2,18 +2,28 @@ n = int(input())
 
 arr = [0] + list(map(int, input().split()))
 
-#dp[i][j] = i번째 계단까지 고려 개단 오른 횟수 -> 최대 동전 수
-dp = [[0] * 4 for _ in range(n+1)]
+#dp[i][j] = i번째 계단까지 고려 1번오른 칸수 j-> 최대 동전 수
+dp = [[0] * (4) for _ in range(n+1)]
 
 dp[0][0] = 0
+dp[1][1] = arr[1]
 
-for i in range(1,n+1):
-    for j in range(1,4):
-        if i-j<0: continue
-        dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i], dp[i-2][j] + arr[i])
+if n > 1:
+    dp[2][0] = arr[2]
+    dp[2][2] = arr[1] + arr[2]
+
+for i in range(3,n+1):
+    for j in range(4):
+        if dp[i-2][j] != 0 :
+            dp[i][j] = max(dp[i][j], dp[i-2][j] + arr[i])
+        if j and dp[i-1][j-1] != 0:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i])
+        
+        
 
 # for d in dp:
 #     print(*d)
-print(dp[n][3])
+ans = max(dp[n])
+print(ans)
 
 

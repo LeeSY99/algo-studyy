c88e97ea9de42e132bbbb90fd5b4dc7d4b1b082d
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-14 22:15:44 +0900

    Title: 둘 중 하나 잘 고르기, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 250814/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 250814/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -6,13 +6,13 @@ arr = [(0,0)] + [tuple(map(int, input().split())) for _ in range(2*n)]
 #dp[i][j] : i번 실행 빨간색카드 j번 선택 -> 뽑힌 숫자의 합
 dp = [[float('-inf')]*(n+1) for _ in range(2*n + 1)]
 
-for i in range(n+1):
-    dp[0][i] = 0
+dp[0][0] = 0
 
 for i in range(1,2*n+1):
     a, b = arr[i]
     dp[i][0] = dp[i-1][0] + b
-    for j in range(1, n+1):
+    up = min(i,n)
+    for j in range(1, up+1):
         dp[i][j] = max(dp[i-1][j-1] + a, dp[i-1][j] + b)
         
 print(dp[2*n][n])

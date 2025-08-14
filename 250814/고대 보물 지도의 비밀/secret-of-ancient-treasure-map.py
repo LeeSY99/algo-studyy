c0d5bae19232a03606c059aeb867e23e1df5de31
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-14 16:24:10 +0900

    Title: 고대 보물 지도의 비밀, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250814/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -0,0 +1,26 @@
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+#dp[i][j] = i번째 숫자까지 고려, 연속된 음수j개 ->  연속합 최대
+
+dp = [[float('-inf')] * (k+1) for _ in range(n+1)]
+
+dp[0][0] = 0
+
+
+for i in range(1,n+1):
+        if arr[i] >= 0:
+            for j in range(k+1):
+                dp[i][j] = max( dp[i-1][j] + arr[i], arr[i])
+        else:
+            for j in range(1,k+1):
+                dp[i][j] = max(dp[i-1][j-1] + arr[i], arr[i])
+
+   
+# for d in dp:
+#     print(*d)
+ans= 0
+for i in range(1,n+1):
+    for j in range(k+1):
+        ans = max(ans, dp[i][j])
+print(ans)

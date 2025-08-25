209bc0bb2aebc74d6af5f63e00d0b9bc042a3f84
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-25 18:47:32 +0900

    Title: 신전 탐험하기 2, Time: 93ms, Memory: 19MB, Status: Passed - Codetree

--- 250825/신전 탐험하기 2/explore-temple-2.py
+++ 250825/신전 탐험하기 2/explore-temple-2.py
@@ -9,21 +9,28 @@ n = int(input())
 c = [[0,0,0]] + [list(map(int, input().split())) for _ in range(n)] 
 
 
-dp = [[0]*3 for _ in range(n+1)]
+dp = [[[0]*3 for _ in range(3)] for _ in range(n+1)]
+for j in range(3):
+    dp[1][j][j] = c[1][j]
 
-for i in range(1,n+1):
+#dp[i][j][k]: i층  j:1층에서 선택한 방, k: i번째 층에서 선택한 방
+for i in range(2,n+1):
     for j in range(3):
-        for k in range(3): #이전 층
-            if j == k: continue
-            dp[i][j] = max(dp[i][j], dp[i-1][k] + c[i][j])
-            if i == n:
-                for l in range(3):
-                    if j == k or l == j:
-                        continue
-                    dp[i][j] = max(dp[i][j], dp[i-1][k] + c[i][j])
+        for k in range(3): 
+            for l in range(3): #직전 층 
+                if k == l: continue
+                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][l] + c[i][k])
+            
 
+ans = 0
+for j in range(3):
+    for k in range(3):
+        if j == k: continue
+        ans = max(ans, dp[n][j][k])
 
-print(max(dp[n]))
+# for d in dp:
+#     print(*d)
+print(ans)
                     
         
 

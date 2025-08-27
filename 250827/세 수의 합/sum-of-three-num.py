8e58cf4cd68fe276c08970ae1047a76813e8672b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-27 17:39:56 +0900

    Title: 세 수의 합, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 250827/세 수의 합/sum-of-three-num.py
+++ 250827/세 수의 합/sum-of-three-num.py
@@ -6,15 +6,21 @@ count = {}
 
 ans = 0
 for i in range(n):
+    n1 = nums[i] 
+
     for j in range(i+1,n):
-        s = nums[i]+nums[j]
-        diff = k-s
+        n2 = nums[j]
+        diff = k - n1 - n2
         if diff in count:
             ans += count[diff]
+
+    if n1 in count:
+        count[n1] += 1
+    else:
+        count[n1] = 1
+
+    
+         
         
-        if s in count:
-            count[s] += 1
-        else:
-            count[s] = 1
 
 print(ans)

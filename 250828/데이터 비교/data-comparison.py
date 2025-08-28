e4e6d8fcff732b5b941d2fb20e293a708ad0d4ef
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-28 15:54:09 +0900

    Title: 데이터 비교, Time: 106ms, Memory: 30MB, Status: Passed - Codetree

---
+++ 250828/데이터 비교/data-comparison.py
@@ -0,0 +1,13 @@
+n = int(input())
+nums1 = list(map(int, input().split()))
+
+m = int(input())
+nums2 = list(map(int, input().split()))
+
+s1 = set(nums1)
+
+for num in nums2:
+    if num in s1:
+        print(1, end = ' ')
+    else:
+        print(0, end = ' ')

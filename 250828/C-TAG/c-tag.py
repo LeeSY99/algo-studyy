04e5b97160941ee75cee2e81bc0c36abe7144acd
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-28 17:02:55 +0900

    Title: C-TAG, Time: 1084ms, Memory: 23MB, Status: Passed - Codetree

--- 250828/C-TAG/c-tag.py
+++ 250828/C-TAG/c-tag.py
@@ -9,15 +9,14 @@ for i in range(m):
     for j in range(i+1,m):
         for k in range(j+1,m):
             s1 = set()
-            s2 = set()
             for l in range(n):
                 w1 = g1[l][i] + g1[l][j] + g1[l][k]
-                w2 = g2[l][i] + g2[l][j] + g2[l][k]
                 s1.add(w1)
-                s2.add(w2)
+            
             flag = True
-            for w1 in s1:
-                if w1 in s2:
+            for l in range(n):
+                w2 = g2[l][i] + g2[l][j] + g2[l][k]
+                if w2 in s1:
                     flag = False
                     break
             

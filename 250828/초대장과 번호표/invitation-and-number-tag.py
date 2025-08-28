c649a57e3b745b422c5dfaf5959c16f0eea8abcb
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-28 20:44:39 +0900

    Title: 초대장과 번호표, Time: 231ms, Memory: 37MB, Status: Passed - Codetree

--- 250828/초대장과 번호표/invitation-and-number-tag.py
+++ 250828/초대장과 번호표/invitation-and-number-tag.py
@@ -6,14 +6,34 @@ k명인 그룹에서 k-1명이 초대를 받음, 1명은 받아야함
 확실하게 초대장 받게 되는 인원 수 출력'''
 
 n, g = map(int, input().split())
-groups = []
-invited = set([1])
-for _ in range(g):
+groups = [set() for _ in range(g)]  #그룹에 속한 사람 번호
+people_groups = [[] for _ in range(n)]  #사람별로 어디 그룹에 속하는지
+
+count = {}
+for i in range(g):
     num, *people = map(int, input().split())
     for p in people:
-        if p not in invited:
-            invited.add(p)
-            break
+        groups[i].add(p-1)
+        people_groups[p-1].append(i)
 
-print(len(invited))
+from collections import deque
+q = deque()
+invited = [False] * n
+invited[0] = True #첫번째사람은 항상 초다
+q.append(0)
+ans = 0
+while q:
+    x = q.popleft()
+    ans +=1
+    for g_num in people_groups[x]:
+        groups[g_num].remove(x)
+        if len(groups[g_num]) == 1:
+            invite_p = list(groups[g_num])[0]
+            if not invited[invite_p]:
+                invited[invite_p] = True
+                q.append(invite_p)
+
+print(ans)
+
+
 

2c00c9670e28fb232afb490620378de3f988551f
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-29 16:38:22 +0900

    Title: 문제 추천 시스템 1, Time: 609ms, Memory: 28MB, Status: Runtime Error - Codetree

---
+++ 250829/문제 추천 시스템 1/problem-recommendation-system-1.py
@@ -0,0 +1,29 @@
+n= int(input())
+
+from sortedcontainers import SortedSet
+ss = SortedSet()
+
+for _ in range(n):
+    p, l = map(int, input().split())
+    ss.add((l,p))
+
+m = int(input())
+for _ in range(m):
+    cmd, *remain = input().split()
+    if cmd == 'rc':
+        x = int(remain[0])
+        if x == 1:
+            print(ss[-1][1])
+        if x == -1:
+            print(ss[0][1])
+
+    if cmd == 'ad':
+        p, l = int(remain[0]), int(remain[1])
+        idx = ss.bisect_left((l,p))
+        if ss[idx] == (l,p):
+            ss.remove((l,p))
+        ss.add((l,p))
+    
+    if cmd == 'sv':
+        p, l = int(remain[0]), int(remain[1])
+        ss.remove((l,p))

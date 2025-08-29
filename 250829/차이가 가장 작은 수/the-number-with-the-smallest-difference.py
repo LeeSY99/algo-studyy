28b8912433c69bb477831343f40af3702dd47c90
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-08-29 16:13:04 +0900

    Title: 차이가 가장 작은 수, Time: 1097ms, Memory: 30MB, Status: Time Limit Exceed - Codetree

--- 250829/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
+++ 250829/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -1,23 +1,21 @@
 n,m = map(int, input().split())
 from sortedcontainers import SortedSet
 
-ss1 = SortedSet()
-ss2 = SortedSet()
+ss = SortedSet()
 
 ans = float('inf')
 for _ in range(n):
     num = int(input())
-    idx2 = ss2.bisect_left(num+m)
-    idx1 = ss1.bisect_left(-(num-m))
+    idx2 = ss.bisect_left(num+m)
+    idx1 = ss.bisect_right((num-m)) - 1
 
-    if idx1 != len(ss1):
-        ans = min(ans, num + ss1[idx1])
+    if idx1 >= 0:
+        ans = min(ans, num - ss[idx1])
+    
+    if idx2 != len(ss):
+        ans = min(ans, ss[idx2] - num)
 
-    if idx2 != len(ss2):
-        ans = min(ans, ss2[idx2] - num)
-
-    ss1.add(-num)
-    ss2.add(num)
+    ss.add(num)
 
 if ans == float('inf'):
     print(-1)

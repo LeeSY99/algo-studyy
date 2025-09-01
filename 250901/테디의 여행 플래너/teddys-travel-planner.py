ed75388de1667d0731fbf9955be5ffc9970b445c
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-01 17:03:47 +0900

    Title: 테디의 여행 플래너, Time: 100ms, Memory: 71MB, Status: Runtime Error - Codetree

---
+++ 250901/테디의 여행 플래너/teddys-travel-planner.py
@@ -0,0 +1,94 @@
+N,Q = map(int, input().split())
+city = list(input().split())
+class City:
+    def __init__(self,name):
+        self.name = name
+        self.next = None
+        self.prev = None
+#첫도시가 핀셋
+class Dlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def add_back(self, city):
+        if self.count == 0:
+            self.head = city
+            self.tail = city
+        else:
+            self.tail.next = city
+            city.prev = self.tail
+            self.tail = city
+        
+        self.count += 1
+
+    def delete_back(self,pin):
+        target = pin.next
+        pin.next = target.next
+        target.next.prev = pin
+        target.next = None
+        target.prev = None
+        self.count -= 1
+
+    def add_back_pin(self,pin,city):
+        if pin == self.tail:
+            pin.next = city
+            city.prev = pin
+            self.tail = city
+        
+        else:
+            pin.next.prev = city
+            city.next = pin.next
+            pin.next = city
+            city.prev = pin
+        self.count += 1
+            
+
+
+trip = Dlist()
+for c in city:
+    new_city = City(c)
+    trip.add_back(new_city)
+pin = trip.head
+
+
+for _ in range(Q):
+    cmd , *r = input().split()
+
+    if cmd == '1':
+        if pin.next:
+            pin = pin.next
+        else:
+            pin = trip.head
+    elif cmd == '2':
+        if pin.prev:
+            pin = pin.prev
+        else:
+            pin = trip.tail
+
+    elif cmd == '3':
+        if pin.next:
+            trip.delete_back(pin)
+
+    elif cmd == '4':
+        new_city = City(r[0])
+        trip.add_back_pin(pin, new_city)
+
+
+    if pin.prev:
+        prev_city = pin.prev
+    else:
+        prev_city = trip.tail
+    
+    if pin.next:
+        next_city = pin.next
+    else:
+        next_city = trip.head
+
+    if prev_city.name == next_city.name:
+        print(-1)
+    else:
+        print(prev_city.name, next_city.name)
+
+    

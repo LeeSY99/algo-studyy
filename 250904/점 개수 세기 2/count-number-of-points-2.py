f8a5e9313e3e0e3af2933e020f4b10b921f0ba3d
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-04 16:13:08 +0900

    Title: 점 개수 세기 2, Time: 1479ms, Memory: 280MB, Status: Memory Limit Exceed - Codetree

--- 250904/점 개수 세기 2/count-number-of-points-2.py
+++ 250904/점 개수 세기 2/count-number-of-points-2.py
@@ -35,11 +35,11 @@ def get_sum(x1,y1,x2,y2):
     return prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1]
 
 for x1, y1, x2, y2 in queries:
-    x1 = nums.bisect_left(x1)+1
-    y1 = nums.bisect_left(y1)+1
-    x2 = nums.bisect_right(x2)
-    y2 = nums.bisect_right(y2)
+    new_x1  = nums.bisect_left(x1)+1
+    new_y1  = nums.bisect_left(y1)+1
+    new_x2  = nums.bisect_right(x2)
+    new_y2  = nums.bisect_right(y2)
 
-    ans = get_sum(x1, y1, x2, y2)
+    ans = get_sum(new_x1, new_y1, new_x2, new_y2)
     print(ans)
     

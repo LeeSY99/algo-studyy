39b10e62e58a14e89435eb973b02c51042920268
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-04 14:13:09 +0900

    Title: 구간에 속한 문자의 개수, Time: 504ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250904/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 250904/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -4,34 +4,36 @@ grid = [[0]*(M+1)]
 for _ in range(N):
     grid.append([0] + list(input()))
 
-a_sum = [[0]*(M+1) for _ in range(N+1)]
-b_sum = [[0]*(M+1) for _ in range(N+1)]
-c_sum = [[0]*(M+1) for _ in range(N+1)]
-
 for i in range(1,N+1):
     for j in range(1,M+1):
         if grid[i][j] == 'a':
-            a_sum[i][j] = a_sum[i-1][j] + a_sum[i][j-1] - a_sum[i-1][j-1] + 1
-            b_sum[i][j] = b_sum[i-1][j] + b_sum[i][j-1] - b_sum[i-1][j-1]
-            c_sum[i][j] = c_sum[i-1][j] + c_sum[i][j-1] - c_sum[i-1][j-1]
-
+            grid[i][j] = 1
         elif grid[i][j] == 'b':
-            a_sum[i][j] = a_sum[i-1][j] + a_sum[i][j-1] - a_sum[i-1][j-1]
-            b_sum[i][j] = b_sum[i-1][j] + b_sum[i][j-1] - b_sum[i-1][j-1] + 1
-            c_sum[i][j] = c_sum[i-1][j] + c_sum[i][j-1] - c_sum[i-1][j-1]
+            grid[i][j] = 2
         else:
-            a_sum[i][j] = a_sum[i-1][j] + a_sum[i][j-1] - a_sum[i-1][j-1]
-            b_sum[i][j] = b_sum[i-1][j] + b_sum[i][j-1] - b_sum[i-1][j-1] 
-            c_sum[i][j] = c_sum[i-1][j] + c_sum[i][j-1] - c_sum[i-1][j-1] + 1
+            grid[i][j] = 3
 
-def return_value(r1,c1,r2,c2):
-    a_val = a_sum[r2][c2] - a_sum[r1-1][c2] - a_sum[r2][c1-1] + a_sum[r1-1][c1-1]
-    b_val = b_sum[r2][c2] - b_sum[r1-1][c2] - b_sum[r2][c1-1] + b_sum[r1-1][c1-1]
-    c_val = c_sum[r2][c2] - c_sum[r1-1][c2] - c_sum[r2][c1-1] + c_sum[r1-1][c1-1]
-    print(a_val, b_val, c_val)
+prefix_sum = [[[0] * (M+1) for _ in range(N+1)] for _ in range(4)]
+
+for c in range(1,4):
+    for i in range(1,N+1):
+        for j in range(1,M+1):
+            add_val = 0
+            if grid[i][j] == c:
+                add_val = 1
+                
+            prefix_sum[c][i][j] = prefix_sum[c][i-1][j] + prefix_sum[c][i][j-1] - prefix_sum[c][i-1][j-1] + add_val
+
+    
+
+            
+
+def return_value(c,r1,c1,r2,c2):
+    return prefix_sum[c][r2][c2] - prefix_sum[c][r1-1][c2] - prefix_sum[c][r2][c1-1] + prefix_sum[c][r1-1][c1-1]
+
 
 for _ in range(K):
     r1,c1,r2,c2 = map(int, input().split())
-    return_value(r1,c1,r2,c2)
+    print(return_value(1,r1,c1,r2,c2),return_value(2,r1,c1,r2,c2),return_value(3,r1,c1,r2,c2))
     
             

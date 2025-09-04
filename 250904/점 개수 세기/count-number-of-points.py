ac7015dd370d088a726e3ee2db095fe0107a856b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-04 16:37:34 +0900

    Title: 점 개수 세기, Time: 336ms, Memory: 41MB, Status: Passed - Codetree

--- 250904/점 개수 세기/count-number-of-points.py
+++ 250904/점 개수 세기/count-number-of-points.py
@@ -1,31 +1,28 @@
-from sortedcontainers import SortedSet
+from bisect import bisect_left, bisect_right
 n,q = map(int, input().split())
 
 points = list(map(int, input().split()))
 
-nums = SortedSet(points)
+nums = sorted(set(points))
 # for x in points:
 #     nums.add(x)
 query = [tuple(map(int, input().split())) for _ in range(q)]
-mapper = {}
-cnt = 1
-for num in nums:
-    mapper[num] = cnt
-    cnt += 1
+U = len(nums)
+mapper = {v: i+1 for i, v in enumerate(nums)}
 
-prefix_sum = [0] * cnt
+prefix_sum = [0] * (U + 1)
 for x in points:
     nx = mapper[x]
     prefix_sum[nx] += 1
 
-for i in range(1,cnt):
+for i in range(1, U+1):
     prefix_sum[i] += prefix_sum[i-1]
 
 def get_value(a,b):
     return prefix_sum[b]-prefix_sum[a-1]
 
 for a, b in query:
-    new_a = nums.bisect_left(a) + 1
-    new_b = nums.bisect_right(b) 
+    new_a = bisect_left(nums, a) + 1
+    new_b = bisect_right(nums, b) 
     print(get_value(new_a, new_b))
     

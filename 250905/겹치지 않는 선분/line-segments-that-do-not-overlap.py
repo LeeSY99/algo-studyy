04afd5fb9a5aa1f15db7df5594fe93ae8a8822cd
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-05 16:07:51 +0900

    Title: 겹치지 않는 선분, Time: 217ms, Memory: 29MB, Status: Passed - Codetree

--- 250905/겹치지 않는 선분/line-segments-that-do-not-overlap.py
+++ 250905/겹치지 않는 선분/line-segments-that-do-not-overlap.py
@@ -9,22 +9,29 @@ L = [0] * n
 R = [0] * n
 
 for i in range(n):
+    x1, x2 = points[i]
     if i == 0:
-        L[i] = 1
+        L[i] = x2
     else:
-        if points[i][0] >= points[i-1][1] or points[i][1] > points[i-1][1]:
-            L[i] = L[i-1] + 1
-        else:
-            L[i] = L[i-1]
+        L[i] = max(L[i-1], x2)
 
 for i in range(n-1,-1,-1):
+    x1, x2 = points[i]
     if i == n-1:
-        R[i] = 1
+        R[i] = x2
 
     else:
-        if points[i][1] <= points[i+1][0] or points[i][1] < points[i+1][1]:
-            R[i] = R[i+1] + 1
-        else:
-            R[i] = R[i+1]
+        R[i] = min(R[i+1],x2)
 
-print((R[0] + L[0])//2)
+
+ans = 0
+for i in range(n):
+    x1,x2 = points[i]
+    if i>0 and L[i-1] >= x2:
+        continue
+
+    if i<n-1 and R[i+1] <= x2:
+        continue
+    
+    ans +=1
+print(ans)

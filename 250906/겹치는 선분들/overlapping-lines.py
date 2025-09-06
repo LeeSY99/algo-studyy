4267dbf4662d1496018590b829048e0c4dbd3756
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-06 17:04:48 +0900

    Title: 겹치는 선분들, Time: 55ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250906/겹치는 선분들/overlapping-lines.py
+++ 250906/겹치는 선분들/overlapping-lines.py
@@ -7,22 +7,28 @@ for i in range(1,n+1):
     x, dir = input().split()
     x = int(x)
     if dir == 'R':
-        points.append((min(now_x, now_x + x), 1, i))
-        points.append((max(now_x, now_x + x), -1, i))
+        points.append((min(now_x, now_x + x), 1))
+        points.append((max(now_x, now_x + x), -1))
         now_x += x
     else:
-        points.append((min(now_x, now_x - x), 1, i))
-        points.append((max(now_x, now_x - x), -1, i))
+        points.append((min(now_x, now_x - x), 1))
+        points.append((max(now_x, now_x - x), -1))
         now_x -= x
 
 points.sort()
-
+# print(points)
 ans = 0
 seg_cnt = 0
-for x, v, num in points:
+start = float('-inf')
+for x, v in points:
     seg_cnt += v
     if seg_cnt >= 2:
-        ans += 1
+        if start == float('-inf'):
+            start = x
+    else:
+        if start != float('-inf'):
+            ans += x - start
+            start = float('-inf')
             
 
 print(ans)

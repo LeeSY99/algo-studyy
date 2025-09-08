182283955f11da7e25cae94a087c625c57ec0916
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-08 11:48:30 +0900

    Title: 최대 길이를 위해 선분 제거하기, Time: 973ms, Memory: 54MB, Status: Passed - Codetree

--- 250908/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
+++ 250908/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
@@ -9,22 +9,28 @@ for idx, (a, b) in enumerate(segs):
     points.append((b, -1, idx))
 
 points.sort()
-L = []
-R = []
 
-seg_count = 0
+weights = [0] * n #해당 선분 제외시 영향을 주는 정도 -> 걸친 선분이 1개일 때 x-직전x값 
+segs = set()
+total_len = 0
 ans = 0
-for i in range(n):
-    lenth = 0
-    for x, v, idx in points:
-        if idx == i:
-            continue
-        if seg_count == 0:
-            start = x
-        seg_count += v
-        if seg_count == 0:
-            lenth += x-start
-    
-    ans = max(ans, lenth)
+prev_x = -1
 
+for x, v, idx in points:
+    seg_count = len(segs)
+    if seg_count > 0: #총 길이 더함, 구간이 1개 이상일때
+        total_len += x-prev_x
+
+    if seg_count == 1:  # 해당 선분이 빠졌을 떄 전체 길이에서 얼마큼 빠지는지 -> 구간이 딱 1개일떄
+        target_index = list(segs)[0]
+        weights[target_index] += x - prev_x
+
+    if v == 1:
+        segs.add(idx)
+    else:
+        segs.remove(idx)
+    prev_x = x
+
+for weight in weights:
+    ans = max(ans, total_len-weight)
 print(ans)

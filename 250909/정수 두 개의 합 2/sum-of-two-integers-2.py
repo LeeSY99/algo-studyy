e172a1f7fc816b4b45b8531c2109b8d421a9d9b4
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-09 11:33:17 +0900

    Title: 정수 두 개의 합 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250909/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 250909/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -3,14 +3,16 @@ arr = [int(input()) for _ in range(n)]
 
 arr.sort()
 ans = 0
+j = n-1
 
 for i in range(n):
-    if arr[i] > k:
-        break
-    j = i+1
-    while j<n and arr[i] + arr[j] <= k:
-        j += 1
-    ans += j - i -1
+    while i<j and arr[i] + arr[j] >= k:
+        j -= 1
+
+    if i == j: break
+
+    ans += j-i+1
+    
 
 print(ans)
     

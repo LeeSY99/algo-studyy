95564739748e9523be1b549b3bfc6d8fcb2f06f3
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-09 11:00:41 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 45ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250909/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+num_count = [0] * 100001
+
+j = 0
+ans = 0
+for i in range(n):
+    while j<n and num_count[arr[j]] < 1:
+        num_count[arr[j]] += 1
+        j+=1
+    
+    if j == n:
+        break
+
+    ans = max(ans, j-i)
+    num_count[arr[i]] -= 1
+
+print(ans)
+    

6aa13cdf15e6237f588224a889fe14f149f0932b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-09 11:07:00 +0900

    Title: 가장 짧은 부분합, Time: 51ms, Memory: 18MB, Status: Passed - Codetree

--- 250909/가장 짧은 부분합/shortest-subtotal.py
+++ 250909/가장 짧은 부분합/shortest-subtotal.py
@@ -6,12 +6,13 @@ arr = list(map(int, input().split()))
 j = 0
 sum_val = 0
 for i in range(N):
-    while j<N and sum_val + arr[j] < S:
+    while j<N and sum_val < S:
         sum_val += arr[j]
         j += 1
     
     if sum_val < S: break
-    ans = min(ans, j-i+1)
+    
+    ans = min(ans, j-i)
     sum_val -= arr[i]
         
 if ans == float('inf'):

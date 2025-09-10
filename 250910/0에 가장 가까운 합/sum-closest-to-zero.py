02d95eb494ec273b69561e87ba81ea61ed267ab3
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-10 22:05:25 +0900

    Title: 0에 가장 가까운 합, Time: 191ms, Memory: 33MB, Status: Wrong Answer - Codetree

--- 250910/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 250910/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -7,9 +7,10 @@ from sortedcontainers import SortedSet
 ss = SortedSet(arr)
 n = len(ss)
 j = n-1
+# print(ss)
 for i in range(n):
-    while i<j and abs(arr[i] + arr[j]) < ans:
-        ans = abs(arr[i] - arr[j])
+    while i<j and abs(ss[i] + ss[j]) < ans:
+        ans = abs(ss[i] + ss[j])
         j-=1
 
 print(ans)

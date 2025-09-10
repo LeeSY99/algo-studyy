807be79fefba31dbb6eb771e43415e9ac02b8f08
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-10 21:52:46 +0900

    Title: 바구니 안의 사탕, Time: 162ms, Memory: 33MB, Status: Passed - Codetree

--- 250910/바구니 안의 사탕/candy-in-the-basket.py
+++ 250910/바구니 안의 사탕/candy-in-the-basket.py
@@ -4,9 +4,9 @@ for _ in range(n):
     a, x = map(int, input().split())
     candy[x] += a
 
-sum_val = sum(candy[1: 1+k+k +1])
+sum_val = sum(candy[: 1+k+k ])
 ans = sum_val
-for c in range(2+k, 1000001 - k):
+for c in range(1+k, 1000001 - k):
     i = c-k
     j = c+k
     sum_val -= candy[i-1]

6995d7f52d44b801dd3eaf3af49cba80137cc364
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-10 21:58:51 +0900

    Title: 바구니 안의 사탕, Time: 466ms, Memory: 24MB, Status: Passed - Codetree

--- 250910/바구니 안의 사탕/candy-in-the-basket.py
+++ 250910/바구니 안의 사탕/candy-in-the-basket.py
@@ -1,20 +1,33 @@
 n, k = map(int, input().split())
-candy = [0] * 1000001
-for _ in range(n):
-    a, x = map(int, input().split())
-    candy[x] += a
+# candy = [0] * 1000001
+# for _ in range(n):
+#     a, x = map(int, input().split())
+#     candy[x] += a
 
-sum_val = sum(candy[: 1+k+k ])
-ans = sum_val
-for c in range(1+k, 1000001 - k):
-    i = c-k
-    j = c+k
-    sum_val -= candy[i-1]
-    sum_val += candy[j]
-    ans = max(ans, sum_val)
+# sum_val = sum(candy[: 1+k+k ])
+# ans = sum_val
+# for c in range(1+k, 1000001 - k):
+#     i = c-k
+#     j = c+k
+#     sum_val -= candy[i-1]
+#     sum_val += candy[j]
+#     ans = max(ans, sum_val)
     
 
+candies = []
+for _ in range(n):
+    cnt, x = map(int, input().split())
+    candies.append((x,cnt))
 
+candies.sort()
+ans = 0 
+total_val = 0
+j = 0
+for i in range(n):
+    while j<n and candies[j][0] - candies[i][0] <= 2*k:
+        total_val += candies[j][1]
+        j+=1
+    ans = max(ans, total_val)
+    total_val -= candies[i][1]
 print(ans)
-
 

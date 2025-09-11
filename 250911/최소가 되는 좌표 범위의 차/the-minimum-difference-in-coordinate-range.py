bd7613b57c107275b66eb4c71e04eed5c65591dd
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-11 10:44:22 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250911/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 250911/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -1,19 +1,24 @@
 n, d = map(int, input().split())
 points = [tuple(map(int, input().split())) for _ in range(n)]
-points.sort()
+points.sort(lambda x: (x[1],x[0]))
 
-j = n-1
+j = 1
 i=0
-ans = -1
-while i<j:
+ans = float('inf')
+# print(points)
+while j<n:
     y1 = points[i][1]
     y2 = points[j][1]
 
     if abs(y2-y1) >= d:
-        ans = points[j][0] - points[i][0]
-        j-=1
+        ans = min(ans, abs(points[j][0] - points[i][0]))
+        j+=1
     else:
         i+=1
+        j=i+1
+    
+if ans == float('inf'):
+    ans = -1    
 
 print(ans)
         

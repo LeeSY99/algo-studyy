0bb2db6fbec41cc599919e8d99a2dd7ed603250c
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-11 14:49:24 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 250911/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
+++ 250911/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
@@ -1,14 +1,15 @@
-s = input().strip()
-arr = [ord(c.lower()) - ord('a') for c in s if c.isalpha()]
-n = len(arr)
+word = list(input())
+n = len(word)
+count = {}
 
-if n == 0:
-    print(0)
-else:
-    best = 1
-    cur_l = 0
-    for r in range(1, n):
-        if arr[r] != arr[r-1] + 1:
-            cur_l = r
-        best = max(best, r - cur_l + 1)
-    print(best)
+ans = 0
+j = 0
+for i in range(n):
+    while j<n and word[j] not in count:
+        count[word[j]] = 1
+        j+=1
+    
+    ans = max(ans, j-i)
+    count.pop(word[i])
+
+print(ans)

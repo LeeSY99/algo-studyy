04c9eab8e7791430aa6d1c83d8e4208ee225c934
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-16 11:30:15 +0900

    Title: 가장 가까운 두 점 사이의 거리를 최대화하기, Time: 108ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 250916/가장 가까운 두 점 사이의 거리를 최대화하기/maximize-dist-of-nearest-points.py
@@ -0,0 +1,37 @@
+'''  선분 위 하나 점을 잡아 x축 기준으로
+가장 가까운 두 점 사이의 거리 최대화
+'''
+
+n = int(input())
+
+lines = []
+for _ in range(n):
+    x1,x2 = map(int, input().split())
+    lines.append((x1,x2))
+
+lines.sort()
+
+def check(dist):
+    start = float('-inf')
+    count = 0
+
+    for a, b in lines:
+        while start + dist <= b:
+            start = max(a, start+dist)
+            count += 1
+
+    return count >= n
+
+left = 1
+right = 10**9
+ans = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    if check(mid):
+        left = mid + 1
+        ans = max(ans, mid)
+    else:
+        right = mid - 1
+
+print(ans)

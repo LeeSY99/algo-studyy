903b772900ee3730a91444f4baf58a4d7a71107b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-16 12:35:53 +0900

    Title: 색칠된 칸으로만 이동 2, Time: 266ms, Memory: 23MB, Status: Passed - Codetree

--- 250916/색칠된 칸으로만 이동 2/move-to-the-colored-space-only-2.py
+++ 250916/색칠된 칸으로만 이동 2/move-to-the-colored-space-only-2.py
@@ -1,13 +1,14 @@
 m,n = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(m)]
-colored = [[False] * n for _ in range(m)]
+colored = []
 from collections import deque
 
 for i in range(m):
     arr = list(map(int, input().split()))
     for j in range(n):
         if arr[j] == 1:
-            colored[i][j] = True
+            colored.append((i,j))
+
 
 def in_range(r,c):
     return 0<=r<m and 0<=c<n
@@ -18,10 +19,8 @@ drs, dcs = [0,1,0,-1],[1,0,-1,0]
 
 def check(D):
     visited = [[False] * n for _ in range(m)]
-    for i in range(m):
-        for j in range(n):
-            if colored[i][j] and not visited[i][j]:
-                bfs(i,j,visited,D)
+    i,j = colored[0]
+    bfs(i,j,visited,D)
     
     if DEBUG:
         if D < 30:
@@ -29,18 +28,16 @@ def check(D):
             for v in visited:
                 print(*v)
 
-    for i in range(m):
-        for j in range(n):
-            if colored[i][j] and not visited[i][j]:
-                return False
-    
+    for i, j in colored:
+        if  not visited[i][j]:
+            return False
     return True
 
 
 def bfs(r,c,visited, D):
     q = deque()
     q.append((r,c))
-    # visited[r][c] = True
+    visited[r][c] = True
 
     while q:
         cr,cc = q.popleft()

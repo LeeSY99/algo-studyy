65eb3bd6e018f4100d101511833e117058a75a92
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-22 16:20:43 +0900

    Title: 최대 수 만들기, Time: 1098ms, Memory: 37MB, Status: Time Limit Exceed - Codetree

--- 250922/최대 수 만들기/make-biggest-num.py
+++ 250922/최대 수 만들기/make-biggest-num.py
@@ -2,9 +2,9 @@ from functools import cmp_to_key
 n = int(input())
 arr = [input() for _ in range(n)]
 def compare(x,y):
-    if int(x+y) > int(y+x):
+    if x+y > y+x:
         return -1
-    if int(x+y) < int(y+x):
+    if x+y < y+x:
         return 1
     return 0
 

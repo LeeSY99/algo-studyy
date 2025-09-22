021279bc7a6e1cc583baf4b7065d820da49df00d
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-22 15:26:13 +0900

    Title: 수영장 효율적으로 활용하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250922/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.py
@@ -0,0 +1,37 @@
+n,m = map(int, input().split())
+time = list(map(int, input().split()))
+
+left = 0
+right = 144000000
+ans = float('inf')
+
+def check(t):
+    lane_count = 1
+    j = 0
+    max_time = 0
+    lane_time = 0
+    while j < n:
+        if lane_time + time[j] <= t:
+            lane_time += time[j]
+        else:
+            max_time = max(max_time, lane_time)
+            lane_count += 1
+            lane_time = time[j]
+        j+=1
+    max_time = max(max_time, lane_time)
+
+    return lane_count <= m
+
+
+
+
+while left <= right:
+    mid = (left + right) // 2
+    if check(mid):
+        right = mid - 1
+        ans = min(ans, mid)
+    else:
+        left = mid + 1
+
+print(ans)
+    

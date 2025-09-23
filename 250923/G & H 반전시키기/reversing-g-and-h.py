786a8a6e9de0b4a22b18c0db15994a3137db233e
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-23 15:12:30 +0900

    Title: G & H 반전시키기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250923/G & H 반전시키기/reversing-g-and-h.py
+++ 250923/G & H 반전시키기/reversing-g-and-h.py
@@ -6,11 +6,11 @@ arr2 = list(input())
 start = False
 count = 0
 for i in range(n):
-    if not start and arr1[i] != arr2[2]:
+    if not start and arr1[i] != arr2[i]:
         start = True
         count +=1
         
-    if start and arr1[i] == arr2[2]:
+    if start and arr1[i] == arr2[i]:
         start = False
 
 print(count)

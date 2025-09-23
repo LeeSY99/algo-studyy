0f64508697a4bb7a9e4790082f5574afd3da59c2
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-23 14:37:43 +0900

    Title: 폭탄 해체 작업, Time: 115ms, Memory: 21MB, Status: Passed - Codetree

--- 250923/폭탄 해체 작업/the-bomb-dismantling.py
+++ 250923/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,15 +1,21 @@
-
+import heapq
 n = int(input())
 
 arr = [tuple(map(int, input().split())) for _ in range(n)]
-arr.sort(key = lambda x: (x[1], -x[0]))
-# print(arr)
+arr.sort(key = lambda x: (x[1], x[0]))
+
+
 now = 0
 score = 0
+idx = n-1
+pq = []
 
-for i in range(n):
-    if arr[i][1] > now:
-        now = arr[i][1]
-        score += arr[i][0]
+for t in range(10000,0,-1):
+    while idx >= 0 and arr[idx][1] >= t:
+        heapq.heappush(pq, -arr[idx][0])
+        idx -= 1
+    
+    if pq:
+        score += -heapq.heappop(pq)
 
 print(score)

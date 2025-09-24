11932fa430002d45d0c38d5211365fc9fb9469de
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-24 15:21:03 +0900

    Title: 좌우 반전시키기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250924/좌우 반전시키기/flip-left-and-right.py
+++ 250924/좌우 반전시키기/flip-left-and-right.py
@@ -1,6 +1,8 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
+def in_range(i):
+    return 1<=i<n
 cnt = 0
 for i in range(1, n):
     if arr[i-1] == 0:
@@ -11,9 +13,11 @@ for i in range(1, n):
         else:
             arr[i] = 1
         
-        if arr[i+1] == 1:
+        if in_range(i+1) and arr[i+1] == 1:
             arr[i+1] = 0
-        else:
+        elif in_range(i+1) and arr[i+1]==0:
             arr[i+1] = 1
 
+if sum(arr) != n:
+    cnt = -1
 print(cnt)

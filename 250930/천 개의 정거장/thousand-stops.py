da84818a889e2328ae7c32a1ba779246863e6acf
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-09-30 14:45:49 +0900

    Title: 천 개의 정거장, Time: 208ms, Memory: 27MB, Status: Wrong Answer - Codetree

--- 250930/천 개의 정거장/thousand-stops.py
+++ 250930/천 개의 정거장/thousand-stops.py
@@ -2,7 +2,7 @@
 A,B,N = map(int, input().split())
 graph = [[] for _ in range(1001)]
 
-for line_no in range(N):
+for line_no in range(1,N+1):
     cost, stop_cnt = map(int, input().split())
     stop = list(map(int, input().split()))
 
@@ -10,21 +10,18 @@ for line_no in range(N):
         graph[stop[i]].append((stop[i+1], cost, line_no))
 
 import heapq
-
 def dijkstra(start):
-    costs = [float('inf')] * (1001)
-    costs[start] = 0
-    # print(cost)
-    time = [-1] * (1001)
-    time[start] = 0
-    q = []
-    heapq.heappush(q, (0, start))
-    now_line_no = -1
+    costs = [[float('inf')] * (N+1) for _ in range(1001)]
+    time = [[float('inf')] * (N+1) for _ in range(1001)]
+    costs[start][0] = 0
+    time[start][0] = 0
+    
+    q = [(0, 0, start, 0)] #비용, 시간,  출발, , 노선번호
 
     while q:
-        now_cost, now_stop= heapq.heappop(q)
+        now_cost, now_time, now_stop, now_line_no = heapq.heappop(q)
 
-        if now_cost != costs[now_stop]:
+        if now_cost != costs[now_stop][now_line_no]:
             continue
 
         for next_stop, cost, next_line_no in graph[now_stop]:
@@ -32,17 +29,18 @@ def dijkstra(start):
                 next_cost = now_cost + cost
             else:
                 next_cost = now_cost
-            if next_cost < costs[next_stop]:
-                costs[next_stop] = next_cost
-                time[next_stop] = time[now_stop] + 1
-                heapq.heappush(q, (next_cost, next_stop))
-                now_line_no = next_line_no
-    return costs, time
 
-cost, time = dijkstra(A)
-if cost[B] == float('inf'):
-    print(-1, -1)
-else:
-    print(cost[B], time[B])
+            if next_cost < costs[next_stop][next_line_no]:
+                costs[next_stop][next_line_no] = next_cost
+                time[next_stop][next_line_no] = now_time + 1
+                heapq.heappush(q, (next_cost, now_time+1, next_stop, next_line_no))
+    best_cost = min(costs[B])
+    if best_cost == float('inf'):
+        return -1,-1
+    best_time = min(time[B][j] for j in range(1,N+1) if costs[B][j] == best_cost)
+    return best_cost, best_time
+
+ans_cost, ans_time = dijkstra(A)
+print(ans_cost, ans_time)
 
         

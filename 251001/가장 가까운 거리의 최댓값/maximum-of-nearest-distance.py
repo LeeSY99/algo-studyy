5e063a689c12323f0e9bfa195708fed455c0eebf
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-01 13:35:14 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 3084ms, Memory: 39MB, Status: Time Limit Exceed - Codetree

---
+++ 251001/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.py
@@ -0,0 +1,33 @@
+n,m = map(int, input().split())
+a,b,c = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+INF = float('inf')
+for _ in range(m):
+    s,e,w = map(int, input().split())
+    graph[s].append((e,w))
+    graph[e].append((s,w))
+
+import heapq
+def dijkstra(start):
+    dist = [INF] * (n+1)
+    dist[start] = 0
+    q = [(0, start)]
+
+    while q:
+        now_d, now_node = heapq.heappop(q)
+
+        if now_d != dist[now_node]:
+            continue
+
+        for next_node, w in graph[now_node]:
+            next_d = now_d + w
+            if next_d < dist[next_node]:
+                dist[next_node] = next_d
+                heapq.heappush(q, (next_d, next_node))
+
+    return min(dist[a], dist[b], dist[c])
+
+ans = 0
+for i in range(1, n+1):
+    ans = max(ans, dijkstra(i))
+print(ans)

ccc7bfc3cb793e197a35e6e8ecf2c8a1e094af23
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-12 14:54:57 +0900

    Time: 153 ms (49.4%), Space: 57.2 MB (91.96%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -1,41 +1,51 @@
 class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         n,m = len(boxGrid), len(boxGrid[0])
+        for i in range(n):
+            target = m-1
+            for j in range(m-1,-1,-1):
+                if boxGrid[i][j] == '#':
+                    boxGrid[i][j] = '.'
+                    boxGrid[i][target] = '#'
+                    target -= 1
+                elif boxGrid[i][j] == '*':
+                    target = j-1
+
         newgrid = [[None]*n for _ in range(m)]
 
         for i in range(n):
             for j in range(m):
                 newgrid[j][n-1-i] = boxGrid[i][j]
 
-        n,m = len(newgrid), len(newgrid[0])
-        for j in range(m):
-            write = n-1
-            i = n-1
-            while i >= 0:
-                if newgrid[i][j] == '*':
-                    write = i-1
-                    i -= 1
-                else:
-                    ##돌이 낙하할 구간을 구함
-                    seg_bottom = i
-                    while i>=0 and newgrid[i][j] != '*':
-                        i -= 1
-                    seg_top = i+1
+        # n,m = len(newgrid), len(newgrid[0])
+        # for j in range(m):
+        #     write = n-1
+        #     i = n-1
+        #     while i >= 0:
+        #         if newgrid[i][j] == '*':
+        #             write = i-1
+        #             i -= 1
+        #         else:
+        #             ##돌이 낙하할 구간을 구함
+        #             seg_bottom = i
+        #             while i>=0 and newgrid[i][j] != '*':
+        #                 i -= 1
+        #             seg_top = i+1
 
-                    ##구간 내 돌 개수 구함
-                    stones = 0
-                    for ii in range(seg_top, seg_bottom + 1):
-                        if newgrid[ii][j] == '#':
-                            stones += 1
+        #             ##구간 내 돌 개수 구함
+        #             stones = 0
+        #             for ii in range(seg_top, seg_bottom + 1):
+        #                 if newgrid[ii][j] == '#':
+        #                     stones += 1
                     
-                    ##돌 채우기
-                    write = seg_bottom
-                    for _ in range(stones):
-                        newgrid[write][j] = '#'
-                        write -= 1
-                    ## 빠진 돌 빈칸 매꿈
-                    for ii in range(write, seg_top-1, -1):
-                        newgrid[ii][j] = '.'
+        #             ##돌 채우기
+        #             write = seg_bottom
+        #             for _ in range(stones):
+        #                 newgrid[write][j] = '#'
+        #                 write -= 1
+        #             ## 빠진 돌 빈칸 매꿈
+        #             for ii in range(write, seg_top-1, -1):
+        #                 newgrid[ii][j] = '.'
 
-        print(newgrid)
+        # print(newgrid)
         return newgrid

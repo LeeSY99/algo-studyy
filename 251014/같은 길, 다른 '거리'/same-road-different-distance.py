55fc21e9f7fb050dad7932b0a1846b72731371e9
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-14 15:24:21 +0900

    Title: 같은 길, 다른 '거리', Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251014/같은 길, 다른 '거리'/same-road-different-distance.py
@@ -0,0 +1,75 @@
+n,m = map(int, input().split())
+graph_a = [[] for _ in range(n+1)]
+graph_b = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    u,v, w1, w2 = map(int, input().split())
+    graph_a[u].append((v,w1))
+    graph_b[u].append((v,w2))
+
+import heapq
+
+def dijkstra(graph):
+    dist = [float('inf')] * (n+1)
+    dist[1] = 0
+    q = [(0,1)]
+    path = [0] * (n+1)
+
+    while q:
+        now_d, now_node = heapq.heappop(q)
+
+        if now_d != dist[now_node]:
+            continue
+
+        for next_node, weight in graph[now_node]:
+            next_d = now_d + weight
+            if next_d < dist[next_node]:
+                dist[next_node] = next_d
+                heapq.heappush(q, (next_d, next_node))
+                path[next_node] = now_node
+    
+    return path
+            
+path_a = dijkstra(graph_a)
+path_b = dijkstra(graph_b)
+x = n
+vertices_a = [x]
+while x != 1:
+    x = path_a[x]
+    vertices_a.append(x)
+# print(vertices_a)
+
+x = n
+vertices_b = [x]
+while x != 1:
+    x = path_b[x]
+    vertices_b.append(x)
+# print(vertices_b)
+
+warning_a = 0
+for i in range(len(vertices_a)-1,0,-1):
+    u = vertices_a[i]
+    v = vertices_a[i-1]
+    short = float('inf')
+    short_node = []
+    for next_node, weight in graph_b[u]:
+        if weight < short:
+            short = weight
+            short_node.append(next_node)
+    if v not in short_node:
+        warning_a += 1
+
+warning_b = 0
+for i in range(len(vertices_b)-1,0,-1):
+    u = vertices_b[i]
+    v = vertices_b[i-1]
+    short = float('inf')
+    short_node = []
+    for next_node, weight in graph_a[u]:
+        if weight < short:
+            short = weight
+            short_node.append(next_node)
+    if v not in short_node:
+        warning_b += 1
+
+print(min(warning_a, warning_b))

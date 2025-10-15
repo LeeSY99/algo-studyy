5d50323fdb5f2df6aed3cbd1b1596bb01bb3e649
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-15 15:38:50 +0900

    Title: 이동 가능한 점들의 쌍, Time: 147ms, Memory: 20MB, Status: Wrong Answer - Codetree

--- 251015/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
+++ 251015/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -13,14 +13,17 @@ for _ in range(m):
 for k in range(1,n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
-            if dist[i][k]+dist[k][j] < dist[i][j] and (i<=p or j<=p or k<=p):
+            if dist[i][k]+dist[k][j] < dist[i][j]:
                 dist[i][j] = dist[i][k]+dist[k][j]
 
 cnt = 0
 cost = 0
 for _ in range(q):
     i, j = map(int, input().split())
-    if dist[i][j] == float('inf'):
+    dist_ij = float('inf')
+    for k in range(1, p+1):
+        dist_ij = min(dist_ij, dist[i][k]+dist[k][j])
+    if dist_ij == float('inf'):
         continue
     cnt+=1
     cost += dist[i][j]

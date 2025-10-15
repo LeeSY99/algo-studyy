4f2bd315fe64631b221b62e98fc6bbefe21df3bf
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-15 15:13:10 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 170ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 251015/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -0,0 +1,22 @@
+n,m = map(int, input().split())
+
+dist = [[float('inf')] * (n+1) for _ in range(n+1)]
+for _ in range(m):
+    u,v,d = map(int, input().split())
+    dist[u][v] = min(dist[u][v], d)
+
+for i in range(1,n+1):
+    dist[i][i] = 0
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if dist[i][j] == float('inf'):
+            print(-1, end = ' ')
+        else:
+            print(dist[i][j], end = ' ')
+    print()

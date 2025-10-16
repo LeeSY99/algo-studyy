842e08c86e1265189a8334fe30363a11ec462cfd
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-16 15:26:11 +0900

    Title: 트리의 부모 노드, Time: 422ms, Memory: 40MB, Status: Passed - Codetree

--- 251016/트리의 부모 노드/parent-node-of-the-tree.py
+++ 251016/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,18 +1,21 @@
 n = int(input())
-class Node:
-    def __init__(self, num):
-        self.num = num
-        # self.child = []
-        self.parent = None
-
-nodes = [None] * (n+1)
-for i in range(1,n+1):
-    node = Node(i)
-    nodes[i] = node
+parent = [0] * (n+1)
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n+1)
 
 for _ in range(n-1):
-    a, b = map(int, input().split())
-    nodes[b].parent = a
+    a,b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
 
+def search(x):
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            parent[y] = x
+            search(y)
+
+visited[1] = True
+search(1)
 for i in range(2,n+1):
-    print(nodes[i].parent)
+    print(parent[i])

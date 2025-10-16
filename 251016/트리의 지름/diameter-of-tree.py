cf61c315dc9a39c4490782134e06718556aaac66
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-16 16:07:09 +0900

    Title: 트리의 지름, Time: 433ms, Memory: 40MB, Status: Runtime Error - Codetree

--- 251016/트리의 지름/diameter-of-tree.py
+++ 251016/트리의 지름/diameter-of-tree.py
@@ -1,44 +1,38 @@
 n = int(input())
-import sys
-sys.setrecursionlimit(10000)
+edges =[[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dist = [0] * (n + 1)
 
-edges = [[] for _ in range(n+1)]
-for _ in range(n-1):
-    a,b,c = map(int, input().split())
-    edges[a].append((b,c))
-    edges[b].append((a,c))
+for _ in range(n - 1):
+    x, y, d = tuple(map(int, input().split()))
+    # 간선 정보를 인접리스트에 넣어줍니다.
+    edges[x].append((y, d))
+    edges[y].append((x, d))
 
-longest = 0
-longest_node = None
-dist = 0
-visited = [False] * (n+1)
-def travel(x):
-    global dist, longest, longest_node
-    for y,d in edges[x]:
+def dfs(x, total_dist):
+    for y, d in edges[x]:
         if not visited[y]:
-            dist += d
             visited[y] = True
-            if dist > longest:
-                longest = dist
-                longest_node = y
-            travel(y)
-            dist -= d
-visited[1] = True
-travel(1)
-ans = 0
-dist = 0
-visited = [False] * (n+1)
-# print(longest, longest_node)
+            dist[y] = total_dist + d
+            dfs(y, total_dist + d)
+
 def calc(x):
-    global dist, ans
-    for y,d in edges[x]:
-        if not visited[y]:
-            dist += d
-            visited[y] = True
-            # print(x,y,dist)
-            ans = max(ans, dist)
-            calc(y)
-            dist -= d
-visited[longest_node] = True
-calc(longest_node)
+    for i in range(1,n+1):
+        visited[i] = False
+        dist[i] = 0
+
+    visited[x] = True
+    dist[x] = 0
+    dfs(x,0)
+
+    fatherest = 0
+    fatherest_node = None
+    for i in range(1,n+1):
+        if dist[i] > fatherest:
+            fatherest = dist[i]
+            fatherest_node = i
+    return fatherest_node, fatherest
+
+fatherest_node, _ = calc(1)
+_, ans = calc(fatherest_node)
 print(ans)

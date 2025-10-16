9e872e328cdfc5920f5f0a83adfc4b0c10e1802b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-16 16:37:55 +0900

    Title: 트리 노드 제거, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/트리 노드 제거/remove-tree-node.py
+++ 251016/트리 노드 제거/remove-tree-node.py
@@ -3,25 +3,35 @@ parent = list(map(int, input().split()))
 delete = int(input())
 
 edges = [[] for _ in range(n)]
-for x in range(1,n):
+for x in range(n):
     y = parent[x]
-    edges[x].append(y)
+    if y == -1:
+        continue
+    if x == delete or y == delete:
+        continue
+    edges[y].append(x)
 
+# print(edges)
 def dfs(x):
     global ans
     if x == delete:
         return
     if len(edges[x]) == 0:
+        # print(x, len(edges[x]))
         ans += 1
     for y in edges[x]:
         if not visited[y]:
-            visited = y
-            ans += 1
+            visited[y] = True
             dfs(y)
+
 visited = [False] * n
-visited[0] = True
+for i in range(n):
+    if parent[i] == -1:
+        root = i
+# print('root', root)
+visited[root] = True
 ans = 0
-dfs(0)
+dfs(root)
 print(ans)
 
 

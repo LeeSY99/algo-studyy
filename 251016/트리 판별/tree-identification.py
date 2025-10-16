ec68da87b75dcf89c5bcd7b5d22546919fa7d2d2
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-16 17:23:57 +0900

    Title: 트리 판별, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/트리 판별/tree-identification.py
+++ 251016/트리 판별/tree-identification.py
@@ -7,18 +7,10 @@ for _ in range(m):
     nodes.append(a)
     nodes.append(b)
     edges[a].append(b)
-    
+
 nodes = set(nodes)
 nodes = sorted(nodes)
 in_count = {node:0 for node in nodes}
-
-for start, end_list in edges.items():
-    for end in end_list:
-        in_count[end] += 1
-
-for node, count in in_count.items():
-    if count == 0:
-        root = node
 
 visited = {node:0 for node in nodes}
 def bfs(x):
@@ -28,15 +20,29 @@ def bfs(x):
         bfs(y)
         
 
-# print(in_count)
-# print(root)
-visited[root] = 1
-bfs(root)
-ans = 1
 # print(visited)
-for node, is_visited in visited.items():
-    if is_visited != 1:
-        ans = 0
-        break
+def check():
+    root = None
+    for start, end_list in edges.items():
+        for end in end_list:
+            in_count[end] += 1
 
-print(ans)
+    count = 0
+    for node, count in in_count.items():
+        if count == 0:
+            count+= 1
+            root = node
+        if count > 1:
+            return 0
+    if count != 1:
+        return 0
+
+    # print(in_count)
+    visited[root] = 1
+    bfs(root)
+    for node, visited_count in visited.items():
+        if visited_count == 0:
+            return 0
+    return 1
+
+print(check())

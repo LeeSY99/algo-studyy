c207b4fafb79c355b78924c260ac61c8a979f753
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-20 16:48:48 +0900

    Title: 트리 파악, Time: 344ms, Memory: 33MB, Status: Runtime Error - Codetree

---
+++ 251020/트리 파악/identifying-the-tree.py
@@ -0,0 +1,26 @@
+n = int(input())
+edges = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    u,v = map(int, input().split())
+    edges[u].append(v)
+    edges[v].append(u)
+
+marker = []
+def dfs(x, depth):
+    cnt = 0
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            cnt += 1
+            dfs(y,depth + 1)
+    if cnt == 0:
+        marker.append((x, depth))
+visited = [False] * (n+1)
+visited[1] = True
+dfs(1,0)
+
+ans = 0
+for _, depth in marker:
+    ans += depth
+print(ans%2)

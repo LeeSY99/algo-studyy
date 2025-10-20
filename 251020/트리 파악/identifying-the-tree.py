fb0f8c380d24778bd3c5173164aa1999c462b12a
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-20 17:00:26 +0900

    Title: 트리 파악, Time: 289ms, Memory: 29MB, Status: Passed - Codetree

--- 251020/트리 파악/identifying-the-tree.py
+++ 251020/트리 파악/identifying-the-tree.py
@@ -2,24 +2,25 @@ n = int(input())
 edges = [[] for _ in range(n+1)]
 import sys
 sys.setrecursionlimit(100000)
+
 for _ in range(n-1):
     u,v = map(int, input().split())
     edges[u].append(v)
     edges[v].append(u)
 
-def dfs(x, depth):
-    global ans
-    cnt = 0
-    for y in edges[x]:
-        if not visited[y]:
-            visited[y] = True
-            cnt += 1
-            dfs(y,depth + 1)
-    if cnt == 0:
-        ans += depth
 
-visited = [False] * (n+1)
-visited[1] = True
+
 ans = 0
-dfs(1,0)
+stack = [(1,0,0)]
+while stack:
+    x,p,d = stack.pop()
+    is_leaf = True
+    for y in edges[x]:
+        if y == p:
+            continue
+        is_leaf = False
+        stack.append((y,x,d+1))
+    if is_leaf:
+        ans += d
+
 print(ans%2)

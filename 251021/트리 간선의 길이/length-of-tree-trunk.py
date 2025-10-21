7bcaeed1987de5a6bef057bd8d38be07a5205c71
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-21 16:27:10 +0900

    Title: 트리 간선의 길이, Time: 379ms, Memory: 278MB, Status: Wrong Answer - Codetree

--- 251021/트리 간선의 길이/length-of-tree-trunk.py
+++ 251021/트리 간선의 길이/length-of-tree-trunk.py
@@ -17,15 +17,17 @@ def search(start, x):
             dist[start][y] = dist[start][x] + d
             search(start,y)
 
-
+visited = [False] * (n+1)
+search(1, 1)
+fatherest = 0
+fatherest_node = 0
 for i in range(1,n+1):
-    visited = [False] * (n+1)
-    visited[i] = True
-    search(i, i)
+    if dist[1][i] > fatherest:
+        fatherest_node = i
 
-
-ans = 0
-for d in dist:
-    d_max = max(d)
-    ans = max(ans, d_max)
+visited = [False] * (n+1)
+dist = [[0] * (n+1) for _ in range(n+1)]
+search(fatherest_node, fatherest_node)
+ans = max(dist[fatherest_node])
 print(ans)
+

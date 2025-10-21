7be0bf17abb5c02d968b7a990d2fee700cb68e9d
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-21 17:01:19 +0900

    Title: 트리 최적의 노드, Time: 335ms, Memory: 35MB, Status: Runtime Error - Codetree

--- 251021/트리 최적의 노드/tree-optimal-node.py
+++ 251021/트리 최적의 노드/tree-optimal-node.py
@@ -11,19 +11,30 @@ def search(x):
         if visited[y]: continue
         visited[y] = True
         dist[y] = dist[x] + 1
-        if dist[y] > max_dist:
-            max_dist = dist[y]
-            max_node = y
+        # if dist[y] > max_dist:
+        #     max_dist = dist[y]
+        #     max_node = y
         search(y)
 
-ans = float('inf')
-for i in range(1,n+1):
-    visited = [False] * (n+1)
-    visited[i] = True
-    dist = [0] * (n+1)
-    max_dist = 0
-    max_node = 0
-    search(i)
-    ans = min(ans, max_dist)
 
-print(ans)
+visited = [False] * (n+1)
+visited[1] = True
+dist = [0] * (n+1)
+search(1)
+# print(dist)
+
+fatherest = 0
+fatherest_node = None
+for i in range(1,n+1):
+    if dist[i] >  fatherest:
+        fatherest = dist[i]
+        fatherest_node = i
+
+# print(fatherest_node)
+visited = [False] * (n+1)
+visited[fatherest_node] = True
+dist = [0] * (n+1)
+search(fatherest_node)
+print((max(dist) + 1 )//2)
+
+

83ae3473637b621f40b80d666ade6a9cd3764b96
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-28 15:23:52 +0900

    Title: 트리 순회, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251028/트리 순회/the-tree-traversal.py
@@ -0,0 +1,53 @@
+n = int(input())
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.left = None
+        self.right = None
+
+nodes = {}
+
+for _ in range(n):
+    n, l, r = input().split()
+    if n not in nodes:
+        node = Node(n)
+        nodes[n] = node
+    else:
+        node = nodes[n]
+    if l != '.':
+        left = Node(l)
+        node.left = left
+        nodes[l] = left
+    if r != '.':
+        right = Node(r)
+        node.right = right
+        nodes[r] = right
+
+
+def preorder(x):
+    if x == None:
+        return
+    print(x.id, end = '')
+    preorder(x.left)
+    preorder(x.right)
+
+def inorder(x):
+    if x == None:
+        return
+    inorder(x.left)
+    print(x.id, end = '')
+    inorder(x.right)
+
+def postorder(x):
+    if x == None:
+        return
+    postorder(x.left)
+    postorder(x.right)
+    print(x.id, end = '')
+
+preorder(nodes['A'])
+print()
+inorder(nodes['A'])
+print()
+postorder(nodes['A'])
+print()

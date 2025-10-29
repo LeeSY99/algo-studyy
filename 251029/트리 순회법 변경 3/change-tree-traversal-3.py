6260914b11c6b436d25a1f2f3d19dac6b8fd1609
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-29 16:18:39 +0900

    Title: 트리 순회법 변경 3, Time: 276ms, Memory: 42MB, Status: Passed - Codetree

--- 251029/트리 순회법 변경 3/change-tree-traversal-3.py
+++ 251029/트리 순회법 변경 3/change-tree-traversal-3.py
@@ -1,28 +1,32 @@
 n = int(input())
 in_order = list(map(int, input().split()))
 post_order = list(map(int, input().split()))
-
+import sys
+sys.setrecursionlimit(100000)
 in_order_map = {}
 for i in range(n):
     in_order_map[in_order[i]] = i 
 
+pre_order = [0] * n
+idx = 0
 
-post_idx = n-1
-
-def build(in_l, in_r):
-    global post_idx
-    if in_l > in_r:
-        return []
+def build(in_l, in_r, po_l, po_r):
+    global idx
+    if po_l > po_r:
+        return
     
-    root = post_order[post_idx]
-    post_idx -= 1
-
+    root = post_order[po_r]
     mid = in_order_map[root]
 
-    right_pre = build(mid+1, in_r)
-    left_pre = build(in_l, mid-1)
-    return [root] + left_pre + right_pre
+    left_size = mid - in_l
+    right_size = in_r - mid
 
-pre_order = build(0,n-1)
+    pre_order[idx] = root
+    idx += 1
+    build(in_l, mid-1, po_l, po_l + left_size-1)
+    build(mid+1, in_r, po_l + left_size, po_r-1)
+    
+
+build(0,n-1,0,n-1)
 print(*pre_order)
 

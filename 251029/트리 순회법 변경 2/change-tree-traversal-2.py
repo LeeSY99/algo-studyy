afef15861050785d0d9c13477a85f069aab32731
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-10-29 15:57:01 +0900

    Title: 트리 순회법 변경 2, Time: 239ms, Memory: 39MB, Status: Passed - Codetree

--- 251029/트리 순회법 변경 2/change-tree-traversal-2.py
+++ 251029/트리 순회법 변경 2/change-tree-traversal-2.py
@@ -2,24 +2,29 @@ n = int(input())
 pre_res = list(map(int, input().split()))
 in_res = list(map(int, input().split()))
 
-post_res = []
+post_res = [0] * n 
 in_order_map = {}
 for i in range(n):
     in_order_map[in_res[i]] = i
 
-pre_idx = 0
-def build(in_l, in_r):
-    global pre_idx
-    if in_l > in_r:
+cnt = 0
+def build(pr_l, pr_r, in_l, in_r):
+    global cnt
+    if pr_l > pr_r:
         return
 
-    root = pre_res[pre_idx]
-    pre_idx += 1
-
+    root = pre_res[pr_l]
     mid = in_order_map[root]
-    build(in_l, mid-1)
-    build(mid+1, in_r)
-    post_res.append(root)
 
-build(0,n-1)
+    left_size = mid - in_l
+    right_size = in_r - mid
+
+    build(pr_l+1, pr_l+left_size, in_l, mid - 1)
+    build(pr_l+left_size + 1, pr_r, mid + 1, in_r)
+    
+    post_res[cnt] = root
+    cnt += 1
+    
+
+build(0,n-1,0,n-1)
 print(*post_res)

b17cb4129976ef76fb373e0f73529ea5aa18a2e8
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-01 15:48:19 +0900

    Title: 트리 전달, Time: 257ms, Memory: 36MB, Status: Runtime Error - Codetree

--- 251101/트리 전달/tree-forwarding.py
+++ 251101/트리 전달/tree-forwarding.py
@@ -1,8 +1,20 @@
 n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
 parent = [0] + list(map(int, input().split()))
+for y in range(1,n+1):
+    x = parent[y]
+    if x == -1:
+        continue
+    graph[x].append(y)
+    
+
 score = [0] * (n+1)
 for _ in range(m):
     i,w = map(int, input().split())
-    score[i] += score[parent[i]] + w
-
+    score[i] +=  w
+def dfs(x):
+    for y in graph[x]:
+        score[y] += score[x]
+        dfs(y)
+dfs(1)
 print(*score[1:])

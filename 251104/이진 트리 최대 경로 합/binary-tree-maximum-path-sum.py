8c73d2657dcddd02d2d424444ae5d02eaf1d9061
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-04 16:48:33 +0900

    Title: 이진 트리 최대 경로 합, Time: 254ms, Memory: 28MB, Status: Passed - Codetree

--- 251104/이진 트리 최대 경로 합/binary-tree-maximum-path-sum.py
+++ 251104/이진 트리 최대 경로 합/binary-tree-maximum-path-sum.py
@@ -1,47 +1,65 @@
 n = int(input())
-
-
-class Node:
-    def __init__(self, idx):
-        self.idx = idx
-        self.left = None
-        self.right = None
-nodes = [None] * (n+1)
+graph = [[] for _ in range(n+1)]
 
 for _ in range(n-1):
-    a, b = map(int, input().split())
-    node = nodes[a]
-    if node == None:
-        node = Node(a)
-        nodes[a] = node
-    
-    b_node = Node(b)
-    nodes[b] = b_node
-    if node.left == None:
-        node.left = b_node
-    else:
-        node.right = b_node
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
+
+dp = [0] * (n + 1)
 nums = [0]
-for _ in range(n):
-    nums.append(int(input()))
+for i in range(1, n+1):
+    num = int(input())
+    nums.append(num)
 
-ans = 0
-def get_path(root):
+def dfs(x):
     global ans
-    if root == None:
-        return 0
+    visited[x] = True
+    child = []
+    for y in graph[x]:
+        if visited[y] : continue
+        dfs(y)
+        child.append(y)
+    left = child[0] if len(child) >0 else 0
+    right = child[1] if len(child )>1 else 0
 
-    left_path = get_path(root.left)
-    right_path = get_path(root.right)
+    dp[x] =  nums[x] + max(0, dp[left], dp[right])
+    ans = max(ans, nums[x] + max(0, dp[left]) + max(0, dp[right]))  
 
-    one_part_max = max(nums[root.idx] + left_path, nums[root.idx] + right_path)
-    two_part_max = nums[root.idx] + left_path + right_path
-    ans = max(ans, one_part_max, two_part_max)
-
-    return max(one_part_max, 0)
-# print(nodes)
-get_path(nodes[1])
+visited = [False] * (n+1)
+ans = float('-inf')
+dfs(1)
 print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 'ans = float('-inf')
+# def get_path(root):
+#     global ans
+#     if root == None:
+#         return 0
+
+#     left_path = get_path(root.left)
+#     right_path = get_path(root.right)
+
+#     one_part_max = max(root.num + left_path, root.num + right_path)
+#     two_part_max = root.num + left_path + right_path
+#     ans = max(ans, one_part_max, two_part_max)
+
+#     return max(one_part_max, 0)
+# # print(nodes)
+# get_path(nodes[1])
+# print(ans)'
 
 

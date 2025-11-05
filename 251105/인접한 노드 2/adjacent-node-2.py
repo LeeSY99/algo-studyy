d9d9d4a6c41aa58c1a82af661083e42adc36eb9e
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-05 15:43:07 +0900

    Title: 인접한 노드 2, Time: 194ms, Memory: 25MB, Status: Runtime Error - Codetree

--- 251105/인접한 노드 2/adjacent-node-2.py
+++ 251105/인접한 노드 2/adjacent-node-2.py
@@ -16,13 +16,33 @@ def dfs(x):
     for y in graph[x]:
         if visited[y]: continue
         dfs(y)
+
         dp[x][0] += max(dp[y][0], dp[y][1])
         dp[x][1] += dp[y][0]
-    if dp[x][0] <= dp[x][1]:
-        selected.append(x)
 
 visited = [False] * (n+1)
 dfs(1)
 print(max(dp[1]))
+
+
+def recover(x, p_choose):
+    visited[x] = True
+    if p_choose:
+        c_choose = False
+    else:
+        c_choose = dp[x][0] < dp[x][1]
+
+    if c_choose:
+        selected.append(x)
+        for y in graph[x]:
+            if visited[y]: continue
+            recover(y, True)
+    else:
+        for y in graph[x]:
+            if visited[y]: continue
+            recover(y, False)
+visited = [False] * (n+1)
+recover(1, False)
 selected.sort()
 print(*selected)
+

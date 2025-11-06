c2a2257d71d249c96fa8526cddf3f58c8e59a7df
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-06 16:07:42 +0900

    Title: 노드 최적의 개수 2, Time: 3080ms, Memory: 48MB, Status: Time Limit Exceed - Codetree

---
+++ 251106/노드 최적의 개수 2/node-best-count-2.py
@@ -0,0 +1,30 @@
+n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+a = list(map(int, input().split()))
+
+dp = [[0]*2 for _ in range(n+1)]
+for node in a:
+    dp[node][1] += 1
+
+def dfs(x):
+    visited[x] = True
+    if x in a:
+        dp[x][1] = 0
+    else:
+        dp[x][1] = 1
+    dp[x][0] = 0 if x not in a else float('inf')
+    for y in graph[x]:
+        if visited[y]: continue
+        dfs(y)
+        if dp[x][0] < float('inf'):
+            dp[x][0] += dp[y][1]
+        dp[x][1] += min(dp[y][0], dp[y][1])
+
+visited = [False]*(n+1)
+dfs(1)
+print(min(dp[1]))

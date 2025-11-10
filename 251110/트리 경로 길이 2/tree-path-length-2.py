3be212b78b61ec3f39799ba185f1355ad5cf268b
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-10 23:57:01 +0900

    Title: 트리 경로 길이 2, Time: 5012ms, Memory: 41MB, Status: Time Limit Exceed - Codetree

---
+++ 251110/트리 경로 길이 2/tree-path-length-2.py
@@ -0,0 +1,63 @@
+import math, sys
+sys.setrecursionlimit(100000)
+n = int(input())
+MAX_H = int(math.log(100000, 2)) + 1
+
+graph = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a,b,v = map(int, input().split())
+    graph[a].append((b,v))
+    graph[b].append((a,v))
+
+def dfs(x):
+    visited[x] = True
+
+    for y,v in graph[x]:
+        if visited[y]: continue
+        parent[0][y] = x
+        depth[y] = depth[x] + 1
+        dfs(y)
+
+parent = [[0] * (n+1) for _ in range(MAX_H+1)]
+visited = [False] * (n+1)
+depth = [0] * (n+1)
+dfs(1)
+
+def lca(a,b):
+    if depth[a] < depth[b]:
+        return lca(b,a)
+
+    for h in range(MAX_H,-1,-1):
+        if depth[a] - depth[b] >= (1<<h):
+            a = parent[h][a]
+    
+    if a == b:
+        return a
+
+    for h in range(MAX_H,-1,-1):
+        if parent[h][a] != parent[h][b]:
+            a = parent[h][a]
+            b = parent[h][b]
+
+    return a
+
+def get_dist(a):
+    visited[a] = True
+    for y,v in graph[a]:
+        if visited[y]: continue
+        dist[y] = dist[a] + v
+        get_dist(y)
+
+for h in range(1, MAX_H+1):
+    for i in range(1,n+1):
+        parent[h][i] = parent[h-1][parent[h-1][i]]
+
+q = int(input())
+for _ in range(q):
+    a,b = map(int, input().split())
+    l = lca(a,b)
+    visited = [False] * (n+1)
+    dist = [0] * (n+1)
+    get_dist(l)
+    print(dist[a]+ dist[b])
+

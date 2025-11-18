143109ddda265c5e063ada601db72e77fc1ce50e
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-18 17:53:48 +0900

    Title: 연결된 정점 2, Time: 2058ms, Memory: 44MB, Status: Time Limit Exceed - Codetree

---
+++ 251118/연결된 정점 2/connected-vertex-2.py
@@ -0,0 +1,31 @@
+n = int(input())
+parent = {}
+size = {}
+
+def make(x):
+    if x not in parent:
+        parent[x] = x
+        size[x] = 1
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a,b):
+    make(a)
+    make(b)
+    A = find(a)
+    B = find(b)
+    if A==B: return 
+    if size[B] < size[B]:
+        A,B = B,A
+    parent[B]=A
+    size[A] += size[B]
+
+    
+for _ in range(n):
+    a,b = map(int, input().split())
+    union(a,b)
+    A = find(a)
+    print(size[A])

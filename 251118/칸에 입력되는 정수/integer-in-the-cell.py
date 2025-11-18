425fb9fa993d5daf3ed0e601ac62124384b092ed
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-11-18 16:23:43 +0900

    Title: 칸에 입력되는 정수, Time: 119ms, Memory: 20MB, Status: Runtime Error - Codetree

---
+++ 251118/칸에 입력되는 정수/integer-in-the-cell.py
@@ -0,0 +1,22 @@
+n = int(input())
+m = int(input())
+
+
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+parent = [i for i in range(n+1)]
+for i in range(1, m+1):
+    a = int(input())
+    A = find(a)
+    if A == 0:
+        ans = i-1
+        break
+    Y = find(A-1)
+    parent[A] = Y
+
+print(ans)
+    

92fca433294e14db9a833c45b9470c85e58abaac
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-12-01 17:10:43 +0900

    Title: 원형 이동, Time: 97ms, Memory: 29MB, Status: Passed - Codetree

--- 251201/원형 이동/circular-movement.py
+++ 251201/원형 이동/circular-movement.py
@@ -1,19 +1,39 @@
 n,m,k = map(int, input().split())
 cost_from_center = [0] + list(map(int, input().split()))
 graph = [[] for _ in range(n+1)]
+blocked = [False] * (n+1)
+
+for i in range(1,n+1):
+    graph[0].append((i, cost_from_center[i]))
+    graph[i].append((0, cost_from_center[0]))
 
 for _ in range(m):
     a,b = map(int, input().split())
-    cost = cost_from_center[a]+cost_from_center[b]
-    graph[a].append((b, cost))
+    if a>b:
+        a,b = b,a
+
+    if a==1 and b == n:
+        blocked[b] = True
+    else:
+        blocked[a] = True
+
+for i in range(1, n+1):
+    if blocked[i]:
+        continue
+    a = i
+    b = i+1
+    if a == n:
+        b = 1
+    graph[a].append((b, 0))
+    graph[b].append((a, 0))
 
 import heapq
 
 def prim():
-    q = [(0,1)]
+    q = [(0,0)]
     dist = [float('inf')] * (n+1)
     visited = [False] * (n+1)
-    dist[1] = 0
+    dist[0] = 0
     cost =0
 
     while q:
@@ -30,6 +50,9 @@ def prim():
                 dist[next_node] = next_d
                 heapq.heappush(q, (next_d, next_node))
     return cost
+if m <= 1:
+    print(1)
+    exit(0)
 
 if prim() > k:
     print(0)

054b226e8b2d74819629b064991b8c909745cd4d
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-12-02 03:31:33 +0900

    Title: 친구의 키, Time: 178ms, Memory: 26MB, Status: Passed - Codetree

---
+++ 251202/친구의 키/height-of-friends.py
@@ -0,0 +1,27 @@
+from collections import deque
+n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+in_degree = [0] * (n+1)
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    in_degree[b] += 1
+
+q = deque()
+for i in range(1,n+1):
+    if in_degree[i] == 0:
+        q.append(i)
+
+order = []
+while q:
+    now_node = q.popleft()
+    order.append(now_node)
+    for y in graph[now_node]:
+        in_degree[y] -=1
+        if in_degree[y] == 0:
+            q.append(y)
+
+print(*order)
+
+

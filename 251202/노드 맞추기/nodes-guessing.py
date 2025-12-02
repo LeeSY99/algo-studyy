108b9eb64b6c21846b5681873e80ab899a39cda2
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-12-02 17:12:27 +0900

    Title: 노드 맞추기, Time: 595ms, Memory: 27MB, Status: Passed - Codetree

--- 251202/노드 맞추기/nodes-guessing.py
+++ 251202/노드 맞추기/nodes-guessing.py
@@ -1,35 +1,53 @@
 n = int(input())
 root = list(input().split())
 m = int(input())
-graph = {}
-in_degree = {}
+graph = [[] for _ in range(n+1)]
+in_degree = [0] * (n+1)
+name_to_id = {}
+id_to_name = {}
+root.sort()
+
+for i in range(n):
+    name_to_id[root[i]] = i
+    id_to_name[i] = root[i]
 
 for _ in range(m):
     x,y = input().split()
-    graph.setdefault(y, []).append(x)
-    in_degree[x] = in_degree.get(x, 0) + 1
+    x_id = name_to_id[x]
+    y_id = name_to_id[y]
+    graph[y_id].append(x_id)
+    in_degree[x_id] += 1
 
 import heapq
 q = []
 for r in root:
-    if in_degree.get(r, 0) == 0:
-        heapq.heappush(q, r)
+    r_id = name_to_id[r]
+    if in_degree[r_id]==0:
+        heapq.heappush(q, r_id)
 
 print(len(q))
-print(*q)
-tree = {}
+for r in q:
+    print(id_to_name[r], end = ' ')
+print()
+# print(graph)
+tree = [[] for _ in range(n+1)]
 while q:
-    x = heapq.heappop(q)
+    x_id = heapq.heappop(q)
 
-    for y in graph.get(x, []):
-        in_degree[y] -= 1
-        if in_degree[y] == 0:
-            heapq.heappush(q,y)
-            tree.setdefault(x, []).append(y)
+    for y_id in graph[x_id]:
+        in_degree[y_id] -= 1
+        if in_degree[y_id] == 0:
+            heapq.heappush(q, y_id)
+            tree[x_id].append(y_id)
 
-root.sort()
+
 for r in root:
-    children = sorted(tree.get(r, []))
-    print(r, len(children), *children)
+    r_id = name_to_id[r]
+    children = sorted(tree[r_id])
+    print(r, end = ' ')
+    print(len(children), end = ' ')
+    for c_id in children:
+        print(id_to_name[c_id], end = ' ')
+    print()
 
 

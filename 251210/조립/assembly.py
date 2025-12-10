f15bc7bc452d1db4e12b2f3ce487828bdcd680d5
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-12-10 23:54:52 +0900

    Title: 조립, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 251210/조립/assembly.py
+++ 251210/조립/assembly.py
@@ -2,13 +2,12 @@ n,m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
 indegree = [0] * (n+1)
-is_mini = [False] * (n+1)
 dp = [[0] * (n+1) for _ in range(n+1)]
 # dp[i][j] = i번 조각을 만드는데 필요한 j번 조각 개수
 
 for _ in range(m):
     m_num, s_num, cnt = map(int, input().split())
-    dp[m_num][s_num] = cnt
+    # dp[m_num][s_num] = cnt
     graph[s_num].append((m_num, cnt))
     indegree[m_num] += 1
     
@@ -17,13 +16,14 @@ q = []
 for i in range(1, n+1):
     if indegree[i] == 0:
         heapq.heappush(q, i)
-        is_mini[i] = True
+        dp[i][i] = 1
 
 while q:
     x = heapq.heappop(q)
 
     for y, cnt in graph[x]:
         indegree[y] -= 1
+
         for j in range(1,n+1):
             dp[y][j] += dp[x][j] * cnt
 
@@ -33,6 +33,6 @@ while q:
 # for d in dp:
 #     print(*d)
 for i in range(1, n+1):
-    if is_mini[i]:
+    if dp[n][i]:
         print(i, dp[n][i])
 

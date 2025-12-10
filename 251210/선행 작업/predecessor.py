48b8cebfa1809fd1d6e33637baf33fc9f4ffabca
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-12-10 22:13:17 +0900

    Title: 선행 작업, Time: 121ms, Memory: 24MB, Status: Passed - Codetree

--- 251210/선행 작업/predecessor.py
+++ 251210/선행 작업/predecessor.py
@@ -2,33 +2,34 @@ n = int(input())
 
 graph = [[] for _ in range(n+1)]
 indegree = [0] * (n+1)
-info = [(-1,-1,-1)]
+times = [0] * (n+1)
 
 for i in range(1,n+1):
     time, pre, *pre_num = map(int, input().split())
-    info.append((time, pre, pre_num))
+    times[i] = time
     for p in pre_num:
-        graph[p].append((i, time))
+        graph[p].append((i))
         indegree[i] += 1
 
 import heapq
 q = []
-dp = [float('inf')] * (n+1)
+# dp = [float('inf')] * (n+1)
+dp = [0] * (n+1)
 # print(indegree)
 for i in range(1,n+1):
     if indegree[i] == 0:
         heapq.heappush(q, i)
-        dp[i] = info[i][0]
+        dp[i] = times[i]
 
 while q:
     x = heapq.heappop(q)
-
-    for y, time in graph[x]:
+    for y in graph[x]:
         indegree[y] -= 1
+        dp[y] = max(dp[y], dp[x] + times[y])
         if indegree[y] == 0:
-            dp[y] = min(dp[y], dp[x] + time)
             heapq.heappush(q,y)
 
+# print(dp)
 print(max(dp[1:]))
 
 

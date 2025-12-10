6371a43e8bc10a26fbe47b6e86d045b64f1deb09
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2025-12-10 22:25:06 +0900

    Title: 갈 수 있는 경우의 수, Time: 311ms, Memory: 29MB, Status: Wrong Answer - Codetree

---
+++ 251210/갈 수 있는 경우의 수/possible-path-of-travel.py
@@ -0,0 +1,29 @@
+n,m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    indegree[y] += 1
+
+import heapq
+q = []
+dp = [0] * (n+1)
+for i in range(1,n+1):
+    if indegree[i] == 0:
+        heapq.heappush(q, i)
+dp[1] = 1
+A = 1000000007
+while q:
+    x = heapq.heappop(q)
+    for y in graph[x]:
+        indegree[y] -= 1
+        dp[y] += 1
+        if indegree[y] == 0:
+            dp[y] = max(dp[x], dp[y]) % A
+            
+            heapq.heappush(q, y)
+
+print(dp[n])

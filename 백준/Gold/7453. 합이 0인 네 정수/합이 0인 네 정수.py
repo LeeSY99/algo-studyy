47313d6eb5dcfb4d4868c07ae4b7ae337f57abca
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-01-15 22:46:46 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 8456 ms, Memory: 937564 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+A = []
+B = []
+C = []
+D = []
+
+for _ in range(n):
+    a,b,c,d = map(int, input().split())
+    A.append(a)
+    B.append(b)
+    C.append(c)
+    D.append(d)
+
+dic = {}
+for a in A:
+    for b in B:
+        if a+b in dic:
+            dic[a+b] += 1
+        else:
+            dic[a+b] = 1
+
+ans = 0
+for c in C:
+    for d in D:
+        if -c-d in dic:
+            ans += dic[-c-d]
+print(ans)

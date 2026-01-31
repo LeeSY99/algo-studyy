84eb3f15ad77f776874622e9a22212b6ee899014
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-01-31 17:48:18 +0900

    Time: 87 ms (69.68%), Space: 21 MB (67.95%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,32 +1,29 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        n = len(stones)
+        if len(stones) < 2:
+            return True
+        if stones[1] != 1:
+            return False
+
+        # 가지치기(정석에 자주 포함됨)
+        for i in range(1, len(stones)):
+            if stones[i] - stones[i-1] > i:
+                return False
+
+        stones_set = set(stones)
         dp = defaultdict(set)
-        #dp[i][j] i -> j점프 가능할 경우 점프 거리
-
-        
         dp[0].add(0)
-
-        for i in range(n):
-            stone = stones[i]
-            for last_jump in dp[stone]:
-                for next_jump in [last_jump - 1, last_jump, last_jump + 1]:
-                    if next_jump <= 0:
-                        continue
-                    next_stone = stone + next_jump
-                    if next_stone in stones:
-                        dp[next_stone].add(next_jump)
-        
         last = stones[-1]
-        print(dp)
-        return len(dp[last]) > 0
 
+        for pos in stones:                 # 반드시 "리스트"로 순회(정렬 순서 유지)
+            for k in dp[pos]:
+                for nk in (k-1, k, k+1):
+                    if nk <= 0:
+                        continue
+                    npos = pos + nk
+                    if npos == last:
+                        return True
+                    if npos in stones_set:
+                        dp[npos].add(nk)
 
-
-
-
-        
-        
-            
-
-        
+        return False

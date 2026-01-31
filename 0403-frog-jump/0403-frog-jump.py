7f9e843277d0279d036e2e9de29076042dbe005c
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-01-31 17:51:30 +0900

    Time: 94 ms (67.21%), Space: 21.1 MB (62.78%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -5,25 +5,29 @@ class Solution:
         if stones[1] != 1:
             return False
 
-        # 가지치기(정석에 자주 포함됨)
-        for i in range(1, len(stones)):
-            if stones[i] - stones[i-1] > i:
-                return False
-
         stones_set = set(stones)
         dp = defaultdict(set)
         dp[0].add(0)
         last = stones[-1]
 
-        for pos in stones:                 # 반드시 "리스트"로 순회(정렬 순서 유지)
-            for k in dp[pos]:
-                for nk in (k-1, k, k+1):
-                    if nk <= 0:
+        for stone in stones:
+            for last_jump in dp[stone]:
+                for next_jump in (last_jump-1, last_jump, last_jump+1):
+                    if next_jump <= 0:
                         continue
-                    npos = pos + nk
-                    if npos == last:
+                    next_stone = stone + next_jump
+                    if next_stone == last:
                         return True
-                    if npos in stones_set:
-                        dp[npos].add(nk)
-
+                    if next_stone in stones_set:
+                        dp[next_stone].add(next_jump)
         return False
+
+
+
+
+
+        
+        
+            
+
+        

ebe4080c5f310469490bf2d0fb51206966cb37eb
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-02-18 15:57:30 +0900

    Time: 1019 ms (43.24%), Space: 36.4 MB (60.27%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -2,58 +2,53 @@ class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
 
-        drs, dcs = [0,1,0,-1],[1,0,-1,0]
+        drs,dcs = [0,1,0,-1],[1,0,-1,0]
+
+        size_map = {}
+        index = 2
+        answer = 0
+
         def in_range(r,c):
             return 0<=r<n and 0<=c<n
 
-        def bfs(r,c,num):
-            global ans
-            q = deque([(r,c)])
-            visited[r][c] = True
-            points = [(r,c)]
-            count = 1
-            while q:
-                r,c = q.popleft()
-                for dr,dc in zip(drs,dcs):
-                    nr,nc = r+dr, c+dc
-                    if in_range(nr,nc) and not visited[nr][nc] and grid[nr][nc] == 1:
-                        visited[nr][nc] = True
-                        q.append((nr,nc))
-                        count += 1
-                        points.append((nr,nc))
-            
-            for r,c in points:
-                island[r][c] = (count, num)
-            return count
+        def dfs(r,c,idx):
+            if not in_range(r,c) or grid[r][c] != 1:
+                return 0
+            grid[r][c] = idx
+            cnt = 1
+            cnt += dfs(r,c+1,idx)
+            cnt += dfs(r+1,c,idx)
+            cnt += dfs(r,c-1,idx)
+            cnt += dfs(r-1,c,idx)
+            return cnt
 
-
-        island = [[(0,0)] * n for _ in range(n)]
-        visited = [[False] * n for _ in range(n)]
-        ans = 0
-        num = 1
         for i in range(n):
             for j in range(n):
-                if not visited[i][j] and grid[i][j] == 1:
-                    ans = max(ans, bfs(i,j,num))
-                    num += 1
+                if grid[i][j] == 1:
+                    size = dfs(i,j,index)
+                    size_map[index] = size
+                    answer = max(answer, size)
+                    index +=1
 
-        for r in range(n):
-            for c in range(n):
-                if grid[r][c] == 0:
-                    isl = 1
-                    is_no = []
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    cur = 1
+                    visited = set()
                     for dr, dc in zip(drs,dcs):
-                        nr,nc = r+dr, c+dc
-                        if in_range(nr,nc):
-                            count, num = island[nr][nc]
-                            if num in is_no:
-                                continue
-                            isl += count
-                            is_no.append(num)
-                    print(isl)
-                    ans = max(ans, isl)
+                        nr, nc = i+dr, j+dc
+                        if not in_range(nr,nc):
+                            continue
+                        if grid[nr][nc] == 0:
+                            continue
+                        if grid[nr][nc] in visited:
+                            continue
 
-        return ans
+                        visited.add(grid[nr][nc]) 
+                        cur += size_map[grid[nr][nc]]
+                    answer = max(answer, cur)
+        return answer
+
         
 
         

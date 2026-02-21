d436f2b7d6cde4a857a2b449bf4bf595b45ab6cb
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-02-21 14:49:12 +0900

    Time: 0 ms (100%), Space: 19.7 MB (18.32%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,25 +7,39 @@
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         result = {}
-        nodes = []
+        min_val = float('inf')
+        max_val = float('-inf')
 
         def travel(node, row, col):
+            nonlocal min_val, max_val
             if not node:
                 return
-            nodes.append((col, row, node.val))
+
+            min_val = min(min_val, col)
+            max_val = max(max_val, col)
+            if col not in result:
+                result[col] = [(row, node.val)]
+            else:
+                result[col].append((row, node.val))
+
             travel(node.left, row+1, col-1)
             travel(node.right, row+1, col+1)
 
         travel(root,0,0)
+        print(result)
+        print(min_val, max_val)
+        ans = []
+        for i in range(min_val, max_val+1):
+            if i not in result:
+                # print('i not in result')
+                ans.append([])
+            else:
+                sub = []
+                result[i].sort()
+                for row, num in result[i]:
+                    sub.append(num)
+                ans.append(sub)
+        return ans
+
+
         
-        nodes.sort()
-
-        col_map = defaultdict(list)
-        for col, row, val in nodes:
-            col_map[col].append(val)
-
-        result = []
-        for col in sorted(col_map.keys()):
-            result.append(col_map[col])
-
-        return result

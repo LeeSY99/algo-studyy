99abf5798c8bd9d86321d547d279febbc25f3b3a
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-02-21 14:49:26 +0900

    Time: 0 ms (100%), Space: 19.7 MB (18.32%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -41,5 +41,30 @@ class Solution:
                 ans.append(sub)
         return ans
 
+        '''
+        def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = {}
+        nodes = []
 
+        def travel(node, row, col):
+            if not node:
+                return
+            nodes.append((col, row, node.val))
+            travel(node.left, row+1, col-1)
+            travel(node.right, row+1, col+1)
+
+        travel(root,0,0)
+        
+        nodes.sort()
+
+        col_map = defaultdict(list)
+        for col, row, val in nodes:
+            col_map[col].append(val)
+
+        result = []
+        for col in sorted(col_map.keys()):
+            result.append(col_map[col])
+
+        return result
+        '''
         

34bb32918026f92eefc63dd9ec0322da66e26543
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-03-01 13:07:40 +0900

    Time: 1063 ms (12.89%), Space: 13.6 MB (97.26%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -4,31 +4,50 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        stack = []
+        remove = set()
 
-        open = []
-        close = []
-        result = ""
-        for c in s:
-            if c == '(':
-                open.append(c)
-            elif c == ')':
-                if len(open) > len(close):
-                    close.append(c)
-        open_count = 0
-        for c in s:
-            if c == '(':
-                if close:
-                    result += c
-                    open_count += 1
-                    close.pop()
-            elif c == ')':
-                if open and open_count > 0:
-                    result += c
-                    open_count -= 1
-                    open.pop()
-            else:
-                result += c
+        for i, ch in enumerate(s):
+            if ch == '(':
+                stack.append(i)
+            elif ch == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    remove.add(i)
+
+        remove.update(stack)
+
+        result = ''
+        for i, ch in enumerate(s):
+            if i not in remove:
+                result += ch
+
         return result
+        # open = []
+        # close = []
+        # result = ""
+        # for c in s:
+        #     if c == '(':
+        #         open.append(c)
+        #     elif c == ')':
+        #         if len(open) > len(close):
+        #             close.append(c)
+        # open_count = 0
+        # for c in s:
+        #     if c == '(':
+        #         if close:
+        #             result += c
+        #             open_count += 1
+        #             close.pop()
+        #     elif c == ')':
+        #         if open and open_count > 0:
+        #             result += c
+        #             open_count -= 1
+        #             open.pop()
+        #     else:
+        #         result += c
+        # return result
 
                 
         

1d0a2d246fb90665005c899cd6108a59c76bbf7f
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-03-04 16:29:01 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 169ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 260304/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static int n;
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        String str = sc.next();
+        System.out.println(check(str));
+    }
+
+    public static int check(String str){
+        for (int lenth=1; lenth<n+1; lenth++) {
+            boolean is = false;
+            for(int i=0; i<n-lenth+1; i++) {
+                for(int j=i+lenth; j<n-lenth+1; j++) {
+                    if (str.substring(i,i+lenth).equals(str.substring(j,j+lenth)))
+                        is = true;
+                }
+            }
+            if (is==false) 
+                return lenth;
+        }
+        return n;
+    }
+}

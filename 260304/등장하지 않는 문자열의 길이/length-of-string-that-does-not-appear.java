8da4cc02550e5280cdc6f5798d6aa1de8087d6ad
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2026-03-04 16:30:19 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 227ms, Memory: 20MB, Status: Passed - Codetree

--- 260304/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
+++ 260304/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -13,7 +13,7 @@ public class Main {
         for (int lenth=1; lenth<n+1; lenth++) {
             boolean is = false;
             for(int i=0; i<n-lenth+1; i++) {
-                for(int j=i+lenth; j<n-lenth+1; j++) {
+                for(int j=i+1; j<n-lenth+1; j++) {
                     if (str.substring(i,i+lenth).equals(str.substring(j,j+lenth)))
                         is = true;
                 }
